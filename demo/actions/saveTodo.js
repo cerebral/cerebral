91b1f81f3652da35ad30769f4e4f51cec59d54c1
Alan R. Soares <alanrsoars@gmail.com>
2015-07-06 17:05:31 +1200

    Use arrow functions

--- demo/actions/saveTodo.js
+++ demo/actions/saveTodo.js
@@ -1,11 +1,11 @@
-let saveTodo = function (cerebral, ref) {
-  return new Promise(function (resolve, reject) {
+let saveTodo = (cerebral, ref) => {
+  return new Promise((resolve, reject) => {
     
     let todo = cerebral.get('todos', ref);
     console.log('ref', ref);
     // Simulating posting the todo.data and get an ID from
     // the server. We resolve with the new id and the ref
-    setTimeout(function () {
+    setTimeout(() => {
       console.log('resolving');
       resolve({
         ref: ref,
