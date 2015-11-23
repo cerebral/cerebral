968ae105037491d15e1cb505be95c0846532dc95
Christian Alfoni <christianalfoni@gmail.com>
2015-11-23 10:18:59 +0100

    Fixed env check

--- src/utils.js
+++ src/utils.js
@@ -40,7 +40,7 @@ module.exports = {
     return typeof action === 'function';
   },
   isDeveloping: function () {
-    return !global.process || global.process.env !== 'production';
+    return !global.process || global.process.env.NODE_ENV !== 'production';
   },
   verifyInput: function (actionName, signalName, input, signalArgs) {
     Object.keys(input).forEach(function (key) {
