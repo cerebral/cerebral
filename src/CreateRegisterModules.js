6247af71b977d95eac3c4061740eadc5975e7feb
Christian Alfoni <christianalfoni@gmail.com>
2015-12-30 19:21:05 +0100

    Fixed exposing signals on module

--- src/CreateRegisterModules.js
+++ src/CreateRegisterModules.js
@@ -2,27 +2,29 @@ module.exports = function (controller) {
   return function (modules) {
     Object.keys(modules).forEach(function (moduleName) {
       var module = modules[moduleName];
-      Object.keys(module.signals || {}).forEach(function (key) {
+      var signals = Object.keys(module.signals || {}).reduce(function (signals, key) {
         if (Array.isArray(module.signals[key])) {
           var signalName = moduleName + '.' + key;
-          controller.signal(signalName, module.signals[key]);
+          signals[signalName] = controller.signal(signalName, module.signals[key]);
         }
-      });
-      Object.keys(module.signalsSync || {}).forEach(function (key) {
+        return signals;
+      }, {});
+      signals = Object.keys(module.signalsSync || {}).reduce(function (signals, key) {
         if (Array.isArray(module.signalsSync[key])) {
           var signalName = moduleName + '.' + key;
-          controller.signalSync(signalName, module.signalsSync[key]);
+          signals[signalName] = controller.signalSync(signalName, module.signalsSync[key]);
         }
-      });
+        return signals;
+      }, signals);
       controller.modules[moduleName] = {
-        signals: signals[moduleName],
+        signals: signals,
         services: module.services
       };
       if (typeof module.init === 'function') {
         var meta = module.init({
           controller,
           name: moduleName,
-          signals: signals[moduleName]
+          signals: signals
         });
         if (typeof meta === 'object') {
           Object.keys(meta).forEach(function (key) {
--- tests/modules.js
+++ tests/modules.js
@@ -11,5 +11,28 @@ var Model = function () {
 
 exports['should be able to register a module'] = function (test) {
   var ctrl = Controller(Model);
+  ctrl.register({
+    test: {
+      init: function () {
+
+      }
+    }
+  });
+  test.ok(ctrl.modules.test);
+  test.done();
+};
+
+exports['should pass the controller its name and its signals to the init method'] = function (test) {
+  var ctrl = Controller(Model);
+  test.expect(3);
+  ctrl.register({
+    test: {
+      init: function (args) {
+        test.equal(args.controller, ctrl);
+        test.equal(args.name, 'test');
+        test.deepEqual(args.signals, {});
+      }
+    }
+  });
   test.done();
 };
