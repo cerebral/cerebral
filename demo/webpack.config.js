775b5d214dea1c6fe409d79f89159ed1fbbab539
Aleksey Gurianov <gurianov@gmail.com>
2016-01-03 12:12:49 +0000

    chore(demo): demo config cleanup

---
+++ demo/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var node_modules = path.resolve(__dirname, '..', 'node_modules');
+var TARGET = process.env.npm_lifecycle_event;
+
+var config = {
+  entry: path.resolve(__dirname, 'main.js'),
+  devtool: TARGET === 'demo'? 'eval-source-map' : 'source-map',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'todomvc.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: 'style!css'
+    }, {
+      test: /\.js$/,
+      loader: 'babel?optional=es7.decorators',
+      exclude: node_modules
+    }]
+  }
+};
+
+module.exports = config;
