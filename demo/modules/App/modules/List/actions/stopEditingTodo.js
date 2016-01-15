ebca0b28dfbf0b0d5d3933d854d6ee33be6b7a55
Christian Alfoni <christianalfoni@gmail.com>
2016-01-15 15:09:59 +0100

    Fixed demo

--- demo/modules/App/modules/List/actions/stopEditingTodo.js
+++ demo/modules/App/modules/List/actions/stopEditingTodo.js
@@ -3,10 +3,6 @@ function stopEditingTodo ({input, state, module}) {
   const todoPath = ['todos', input.ref];
   let todo = module.state.get(todoPath);
 
-  if (!todo.$newTitle) {
-    return;
-  }
-
   module.state.merge(todoPath, {
     $isEditing: false
   });
