f63448b7f29a3e508cfb1ab6a9cf068ab3aaf509
Max Frigge <max.frigge@gmail.com>
2016-02-04 09:02:29 +0100

    Better styles for demo

--- packages/cerebral-module-useragent/demo/app/components/App.js
+++ packages/cerebral-module-useragent/demo/app/components/App.js
@@ -4,21 +4,49 @@ import {HOC as Cerebral} from 'cerebral-view-react'
 function App (props) {
   const { useragent } = props
 
+  const containerStyle = {
+    display: 'flex',
+    flexFlow: 'row wrap'
+  }
+
+  const sectionStyle = {
+    whiteSpace: 'pre',
+    background: 'lightgrey',
+    margin: 10,
+    padding: 5
+  }
+
+  const codeStyle = {
+    whiteSpace: 'pre'
+  }
+
   return (
-    <section>
-      <h1>useragent.browser</h1>
-      <code>{ JSON.stringify(useragent.browser) }</code>
-      <h1>useragent.device</h1>
-      <code>{ JSON.stringify(useragent.device) }</code>
-      <h1>useragent.media</h1>
-      <code>{ JSON.stringify(useragent.media) }</code>
-      <h1>useragent.os</h1>
-      <code>{ JSON.stringify(useragent.os) }</code>
-      <h1>useragent.window</h1>
-      <code>{ JSON.stringify(useragent.window) }</code>
-      <h1>useragent.feature</h1>
-      <code>{ JSON.stringify(useragent.feature) }</code>
-    </section>
+    <div style={ containerStyle }>
+      <section style={ sectionStyle }>
+        <h1>useragent.browser</h1>
+        <code style={ codeStyle }>{ JSON.stringify(useragent.browser, null, 2) }</code>
+      </section>
+      <section style={ sectionStyle }>
+        <h1>useragent.device</h1>
+        <code style={ codeStyle }>{ JSON.stringify(useragent.device, null, 2) }</code>
+      </section>
+      <section style={ sectionStyle }>
+        <h1>useragent.media</h1>
+        <code style={ codeStyle }>{ JSON.stringify(useragent.media, null, 2) }</code>
+      </section>
+      <section style={ sectionStyle }>
+        <h1>useragent.os</h1>
+        <code style={ codeStyle }>{ JSON.stringify(useragent.os, null, 2) }</code>
+      </section>
+      <section style={ sectionStyle }>
+        <h1>useragent.window</h1>
+        <code style={ codeStyle }>{ JSON.stringify(useragent.window, null, 2) }</code>
+      </section>
+      <section style={ sectionStyle }>
+        <h1>useragent.feature</h1>
+        <code style={ codeStyle }>{ JSON.stringify(useragent.feature, null, 2) }</code>
+      </section>
+    </div>
   )
 }
 
