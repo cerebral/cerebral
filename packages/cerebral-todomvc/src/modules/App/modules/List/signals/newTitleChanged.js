18c0af9c7ab9e14b939369a6a2f43c653ca1a803
Alexey Gurianov <gurianov@gmail.com>
2016-04-11 00:06:39 +0400

    fix: resolve `isSync` option deprecation

--- packages/cerebral-todomvc/src/modules/App/modules/List/signals/newTitleChanged.js
+++ packages/cerebral-todomvc/src/modules/App/modules/List/signals/newTitleChanged.js
@@ -4,5 +4,5 @@ export default {
   chain: [
     setTodoNewTitle
   ],
-  isSync: true
+  immediate: true
 };
--- packages/cerebral-todomvc/src/modules/App/modules/NewTodo/signals/titleChanged.js
+++ packages/cerebral-todomvc/src/modules/App/modules/NewTodo/signals/titleChanged.js
@@ -4,5 +4,5 @@ export default {
   chain: [
     setTitle
   ],
-  isSync: true
+  immediate: true
 };
