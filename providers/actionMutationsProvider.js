5ac953863804ca5b37b91bef708b2213b812d8a7
Christian Alfoni <christianalfoni@gmail.com>
2016-04-21 13:05:33 +0200

    fix(provider): action mutations provider also needs to wrap the select method

--- providers/actionMutationsProvider.js
+++ providers/actionMutationsProvider.js
@@ -3,29 +3,38 @@
   Should evaluate how signals are replayed. Sometimes you want
   to actually run the signals again (recorder)
 */
+
+function wrapMutators (target, mutators, action, rootPath) {
+  return Object.keys(target).reduce(function (target, targetKey) {
+    if (targetKey in mutators) {
+      var originalMutator = target[targetKey]
+      target[targetKey] = function () {
+        var args = [].slice.call(arguments)
+        var path = args.shift()
+        action.mutations.push({
+          name: targetKey,
+          path: typeof path === 'string' ? rootPath.concat(path.split('.')) : rootPath.concat(path),
+          args: args
+        })
+        originalMutator.apply(null, arguments)
+      }
+    }
+    return target
+  }, target)
+}
+
 module.exports = function (context, execution, controller) {
   var model = controller.getModel()
   var action = execution.action
+  var originalSelect = context.state.select
+
   action.mutations = action.mutations || []
+  context.state = wrapMutators(context.state, model.mutators, action, [])
 
-  return Object.keys(context).reduce(function (newContext, key) {
-    newContext[key] = context[key]
-    if (key === 'state') {
-      Object.keys(model.mutators).forEach(function (mutatorKey) {
-        var originalMutator = context[key][mutatorKey]
-        newContext[key][mutatorKey] = function () {
-          var args = [].slice.call(arguments)
-          var path = args.shift()
-          action.mutations.push({
-            name: mutatorKey,
-            path: typeof path === 'string' ? path.split('.') : path,
-            args: args
-          })
-          originalMutator.apply(null, arguments)
-        }
-      })
-    }
+  context.state.select = function (path) {
+    var cursor = originalSelect(path)
+    return wrapMutators(cursor, model.mutators, action, path)
+  }
 
-    return newContext
-  }, {})
+  return context
 }
