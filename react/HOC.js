211ff2c223a9918a103144371fc794202d1af443
Christian Alfoni <christianalfoni@gmail.com>
2016-06-01 13:19:20 +0200

    fix(hoc): fix computed lookup bug

--- react/HOC.js
+++ react/HOC.js
@@ -1,7 +1,8 @@
 var React = require('react');
 
 function hasChanged(path, changes) {
-  return path.split('.').reduce(function (changes, key) {
+  path = Array.isArray(path) ? path : path.split('.');
+  return path.reduce(function (changes, key) {
     return changes ? changes[key] : false;
   }, changes);
 }
@@ -15,23 +16,27 @@ module.exports = function (Component, paths) {
       this.context.controller.on('flush', this.update);
     },
     componentWillUnmount() {
-      this.context.controller.off('flush', this.update);
+      this.context.controller.removeListener('flush', this.update);
     },
     update(changes) {
+      var hasChange = false;
       for (var key in paths) {
         if (
-          (typeof paths[key] === 'object' && paths[key].hasChanged(changes)) ||
-          (typeof paths[key] !== 'object' && hasChanged(paths[key], changes))
+          (paths[key].hasChanged && paths[key].hasChanged(changes)) ||
+          (!paths[key].hasChanged && hasChanged(paths[key], changes))
         ) {
-          return this.forceUpdate();
+          hasChange = true;
         }
+      }
+      if (hasChange) {
+        this.forceUpdate();
       }
     },
     getProps() {
       var controller = this.context.controller;
       var props = this.props || {};
       var propsToPass = Object.keys(paths || {}).reduce(function (props, key) {
-        props[key] = typeof paths[key] === 'object' ? paths[key].get(controller.get()) : controller.get(paths[key]);
+        props[key] = paths[key].hasChanged ? paths[key].get(controller.get()) : controller.get(paths[key]);
         return props
       }, {})
 
