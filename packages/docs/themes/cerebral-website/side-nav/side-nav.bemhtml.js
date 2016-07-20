11915bf3910eb3c080f718a4ae7eb3f05f879899
Alexey Gurianov <gurianov@gmail.com>
2016-07-20 18:54:27 +0400

    feat(side-nav): basic side nav generation

--- packages/docs/themes/cerebral-website/side-nav/side-nav.bemhtml.js
+++ packages/docs/themes/cerebral-website/side-nav/side-nav.bemhtml.js
@@ -1,7 +1,21 @@
 block('side-nav')(
   content()(function () {
-    return [
-      "Tutorial: Introduction"
-    ]
+    return {
+      block: 'menu',
+      mods: { theme: 'islands', size: 'xl', mode: 'radio' },
+      val: [this._layout, this._name].join('/'),
+      content: this._data.documentation.map(function (page) {
+        return {
+          block: 'menu-item',
+          mods: { theme: 'islands', size: 'xl', type: 'link' },
+          val: [page.layout, page.name].join('/'),
+          content: {
+            block: 'link',
+            url: '../documentation/' + page.name+ '.html',
+            content: page.name
+          }
+        }
+      })
+    }
   })
 )
---
+++ packages/docs/themes/cerebral-website/side-nav/side-nav.deps.js
@@ -0,0 +1,6 @@
+({
+  shouldDeps: [
+    { block: 'menu', mods: { theme: 'islands', mode: 'radio' }},
+    { block: 'menu-item', mods: { theme: 'islands', type: 'link' }}
+  ]
+})
