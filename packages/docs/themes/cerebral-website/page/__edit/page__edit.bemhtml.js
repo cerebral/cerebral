be606f66e38fcc5244da785ff2f60a77a75ec943
Alexey Gurianov <gurianov@gmail.com>
2016-07-21 02:02:46 +0400

    feat(page): edit on Github link

---
+++ packages/docs/themes/cerebral-website/page/__edit/page__edit.bemhtml.js
@@ -0,0 +1,18 @@
+block('page').elem('edit')(
+  content()(function () {
+    var url = 'https://github.com/Guria/cerebral-website/edit/master/content/'
+
+    if (this._layout !== 'root') {
+      url += this._layout + '/'
+    }
+
+    url += [this._name, this._lang, 'md'].join('.')
+
+    return {
+      block: 'link',
+      mods: { theme: 'islands' },
+      url: url,
+      content: 'Edit on GitHub'
+    }
+  })
+)
