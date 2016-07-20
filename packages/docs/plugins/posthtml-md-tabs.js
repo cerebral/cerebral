f9bd557277741f7b74181038a4146093a8ad011a
Alexey Gurianov <gurianov@gmail.com>
2016-07-20 22:41:18 +0400

    fix(plugins): do not return null in posthtml plugin

--- packages/docs/plugins/posthtml-md-tabs.js
+++ packages/docs/plugins/posthtml-md-tabs.js
@@ -3,13 +3,13 @@ module.exports = tree => {
   tree.walk(node => {
     if (node.tag === 'h6') {
       tabs.push({ elem: 'tab', name: node.content, content: [] })
-      node = null
+      node = undefined
     } else if (tabs.length && node.tag === 'hr') {
       node = { block: 'tabs', content: tabs }
       tabs = []
     } else if (tabs.length) {
       tabs[tabs.length - 1].content.push(node.content || node)
-      node = null
+      node = undefined
     }
 
     return node
