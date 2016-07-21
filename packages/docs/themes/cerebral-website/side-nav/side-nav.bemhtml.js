4e014c52fb5535f42e47c6e047dc30a92e4446d9
Alexey Gurianov <gurianov@gmail.com>
2016-07-21 14:48:54 +0400

    feat(side-nav): improve menu generation

--- packages/docs/themes/cerebral-website/side-nav/side-nav.bemhtml.js
+++ packages/docs/themes/cerebral-website/side-nav/side-nav.bemhtml.js
@@ -1,21 +1,46 @@
 block('side-nav')(
   content()(function () {
+    var data = this._data
+
     return {
       block: 'menu',
       mods: { theme: 'islands', size: 'xl', mode: 'radio' },
       val: [this._layout, this._name].join('/'),
-      content: this._data.documentation.map(function (page) {
-        return {
-          block: 'menu-item',
-          mods: { theme: 'islands', size: 'xl', type: 'link' },
-          val: [page.layout, page.name].join('/'),
-          content: {
-            block: 'link',
-            url: '../documentation/' + page.name+ '.html',
-            content: page.name
+      content: Object.keys(data).map(function (layout) {
+        if (layout === 'root') return
+        if (data[layout].length === 1) {
+          var page = data[layout][0]
+
+          return {
+            block: 'menu-item',
+            mods: { type: 'link' },
+            val: [page.layout, page.name].join('/'),
+            content: {
+                block: 'link',
+                url: '../' + page.layout + '/' + page.name + '.html',
+                content: layout
+            }
           }
         }
-      })
+        return {
+          elem: 'group',
+          title: layout,
+          content: data[layout].map(function (page) {
+            if (page.name === 'index') return
+
+            return {
+              block: 'menu-item',
+              mods: { type: 'link' },
+              val: [page.layout, page.name].join('/'),
+              content: {
+                 block: 'link',
+                 url: '../' + page.layout + '/' + page.name + '.html',
+                 content: page.name
+              }
+            }
+          }).filter(function (i) { return !!i })
+        }
+      }).filter(function (i) { return !!i }) // fix menu.bemhtml issues
     }
   })
 )
