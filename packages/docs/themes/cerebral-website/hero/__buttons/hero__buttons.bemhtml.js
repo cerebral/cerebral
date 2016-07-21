1225321f8c6a06d63128fdf73b1f6b57d4af6c67
Alexey Gurianov <gurianov@gmail.com>
2016-07-21 15:31:09 +0400

    feat(hero): use links list from md for buttons

--- packages/docs/themes/cerebral-website/hero/__buttons/hero__buttons.bemhtml.js
+++ packages/docs/themes/cerebral-website/hero/__buttons/hero__buttons.bemhtml.js
@@ -1,25 +1,24 @@
 block('hero').elem('buttons')(
   content()(function () {
     var base = this._layout === 'root' ? './' : '../'
-    return [
-      {
-        block: 'button',
-        mods: { theme: 'islands', size: 'xl', type: 'link' },
-        url: 'http://cerebral.github.io/cerebral-todomvc/',
-        text: 'TRY THE DEMO'
-      },
-      {
-        block: 'button',
-        mods: { theme: 'islands', size: 'xl', type: 'link' },
-        url: base + 'documentation/introduction.html',
-        text: 'TUTORIAL'
-      },
-      {
-        block: 'button',
-        mods: { theme: 'islands', size: 'xl', type: 'link', view: 'action' },
-        url: base + 'documentation/get-started.html',
-        text: 'GET STARTED'
-      }
-    ]
+    var links
+
+    try {
+      links = this._data.root[0].content[0].content
+        .filter(function (i) { return i && i.tag})
+        .map(function (li) {
+          var link = li.content[0] 
+          return {
+            block: 'button',
+            mods: { theme: 'islands', size: 'xl', type: 'link' },
+            url: link.attrs.href,
+            text: link.content
+          }
+        })
+    } catch (e) {
+      links = []
+    }
+
+    return links
   })
 )
