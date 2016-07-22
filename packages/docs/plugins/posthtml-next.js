e7ab18dc273302c07b86d392ea5f8002c9d3a355
Alexey Gurianov <gurianov@gmail.com>
2016-07-22 13:00:05 +0400

    fix: make next and side-nav more error friendly

--- packages/docs/plugins/posthtml-next.js
+++ packages/docs/plugins/posthtml-next.js
@@ -1,5 +1,10 @@
 module.exports = tree => {
   tree.match({ content: [{ tag: 'a', content: ['Next'] }] }, node => {
+    if (!(node.content[0] && node.content[0].attrs && node.content[0].attrs.href)) {
+      console.warn('failed to parse a link to next page')
+      return node
+    }
+
     return {
       block: 'next',
       url: node.content[0].attrs.href
--- packages/docs/themes/cerebral-website/next/next.bemhtml.js
+++ packages/docs/themes/cerebral-website/next/next.bemhtml.js
@@ -4,11 +4,17 @@ block('next')(
 
     var urlParts = this.ctx.url.split('/')
 
-    if (urlParts.length === 2) {
-      meta = this._layouts[this._layout][urlParts[1].replace('.html', '')].meta
-    } else {
-      meta = this._layouts[urlParts[1]][urlParts[2].replace('.html', '')].meta
+    try {
+      if (urlParts.length === 2) {
+        meta = this._layouts[this._layout][urlParts[1].replace('.html', '')].meta
+      } else {
+        meta = this._layouts[urlParts[1]][urlParts[2].replace('.html', '')].meta
+      }
+    } catch (e) {
+      console.log('failed to find meta for ' + this.ctx.url)
+      meta = {}
     }
+
     return {
       block: 'link',
       mods: { theme: 'islands', size: 'xl' },
