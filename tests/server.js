ee82114a0ee4e3082d47d9ff28a24228f87fc2df
psypersky <alkazar.r@gmail.com>
2016-07-25 15:44:59 -0500

    test: get server controller state twice to check state overwrites

--- tests/server.js
+++ tests/server.js
@@ -13,4 +13,11 @@ suite['should get nested key'] = function (test) {
   test.done()
 }
 
+suite['should get two states'] = function (test) {
+  var ctrl = ServerController({foo: {some: 'bar'}, 'boo': 'bee'})
+  test.equal(ctrl.get('foo.some'), 'bar')
+  test.equal(ctrl.get('boo'), 'bee')
+  test.done()
+}
+
 module.exports = { accessors: suite }
