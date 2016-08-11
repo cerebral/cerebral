58a94538203f8424e3914b942c252633e53323dd
Christian Alfoni <christianalfoni@gmail.com>
2016-08-12 01:31:58 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # cerebral-scheme-parser
 The scheme parser for operators
+
+```js
+import schemeParser from 'cerebral-scheme-parser'
+
+const string = 'state:foo.{{input:id}}.{{state:foo.bar}}'
+const parsed = schemeParser(string)
+
+parsed.target // "state"
+parsed.value = // foo.{{input:id}}.{{state:foo.bar}}
+
+// getValue iterates the inline schemes so this callback
+// will be called twice
+const newString = parsed.getValue(function (scheme) {
+  scheme.target // "input", then "state"
+  scheme.value // "id", then "foo.bar"
+
+  if (target === 'input') {
+    return 'woop'
+  }
+
+  return 'wuuut?'
+})
+
+newString // foo.woop.wuuut?
+```
+
+You can also do it async:
+
+```js
+import schemeParser from 'cerebral-scheme-parser'
+
+const string = 'state:foo.{{input:id}}'
+const parsed = schemeParser(string)
+
+parsed.getValuePromise(function (scheme) {
+  scheme.target // "input"
+  scheme.value // "id"
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve('bar')
+    }, 100)
+  })
+})
+  .then(function (value) {
+    value // "foo.bar"
+  })
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = function(path) {
+  if (typeof path !== 'string') {
+    return null;
+  }
+
+  if (!path.match(/\:/)) {
+    return null;
+  }
+
+  var value = path.match(/:(.*)/)[1];
+  var inlineSchemes = value.match(/({{.+?}})/g);
+  var schemes = (inlineSchemes || []).map(function(inlineScheme) {
+    var inlineSchemeSanitizied = inlineScheme.replace(/[{}]/g, '');
+
+    return {
+      target: inlineSchemeSanitizied.match(/(^.*):/)[1],
+      value: inlineSchemeSanitizied.match(/:(.*)/)[1]
+    };
+  });
+
+  return {
+    target: path.match(/(^.*):/)[1],
+    value: value,
+    schemes: schemes,
+    getValue: function(cb) {
+      return schemes.reduce(function(schemedValue, scheme, index) {
+        return schemedValue.replace(inlineSchemes[index], cb(scheme));
+      }, value);
+    },
+    getValuePromise: function(cb) {
+      return schemes.reduce(function(schemedValuePromise, scheme, index) {
+        var newValue = null;
+        return schemedValuePromise.then(function(value) {
+          newValue = value;
+          return cb(scheme);
+        })
+        .then(function(schemeValue) {
+          return newValue.replace(inlineSchemes[index], schemeValue);
+        })
+      }, new Promise(function(resolve) {resolve(value)}));
+    }
+  };
+};
