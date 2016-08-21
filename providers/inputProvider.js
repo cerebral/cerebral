c849a6d8fab498cba7e60689fd14e8c3d40a7e3e
Christian Alfoni <christianalfoni@gmail.com>
2016-08-21 16:01:57 +0200

    fix(signalPayload): show warning when passing invalid payload to singal

--- providers/inputProvider.js
+++ providers/inputProvider.js
@@ -8,16 +8,24 @@ module.exports = function (context, execution) {
     execution.payload,
     action.options.defaultInput ? action.options.defaultInput : {}
   ]
-  context.input = utils.merge.apply(null, inputs)
 
-  if (utils.isDeveloping() && action.options.input) {
-    try {
-      JSON.stringify(context.input)
-    } catch (e) {
-      console.log('Not serializable', context.input)
-      throw new Error('Cerebral - Could not serialize input to signal. Please check signal ' + signal.name)
-    }
+  if (
+    utils.isDeveloping() &&
+    execution.payload &&
+    (
+      typeof execution.payload !== 'object' ||
+      (
+        typeof execution.payload === 'object' &&
+        execution.payload !== null &&
+        !Array.isArray(execution.payload) &&
+        execution.payload.constructor.name !== 'Object'
+      )
+    )
+  ) {
+    console.warn('Cerebral - You passed an invalid signal payload to signal "' + signal.name + '", it has to be a plain object or nothing. This is the payload: ', execution.payload)
   }
+
+  context.input = utils.merge.apply(null, inputs)
 
   return context
 }
