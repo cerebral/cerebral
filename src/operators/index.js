54a26a89410462e01207343e9c08004ac45394d6
Fops <fopsdev@gmail.com>
2016-09-29 09:50:00 +0200

    fix: removed copy operator

--- src/operators/index.js
+++ src/operators/index.js
@@ -1,11 +1,10 @@
-import copyExport from './copy'
 import debounceExport from './debounce'
 import filterExport from './filter'
 import setExport from './set'
 import toggleExport from './toggle'
 import whenExport from './when'
 
-export const copy = copyExport
+
 export const debounce = debounceExport
 export const filter = filterExport
 export const set = setExport
