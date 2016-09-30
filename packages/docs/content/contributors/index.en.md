56e0bfb6b7ceba8e758410ae562a38afa4047690
Christian Alfoni <christianalfoni@gmail.com>
2016-09-30 22:31:23 +0200

    Contributors and footer

--- packages/docs/content/contributors/index.en.md
+++ packages/docs/content/contributors/index.en.md
@@ -1,1 +1,3 @@
 # Contributors
+
+![christian](../public/img/christian.jpeg)
--- packages/docs/themes/cerebral-website/footer/footer.bemhtml.js
+++ packages/docs/themes/cerebral-website/footer/footer.bemhtml.js
@@ -11,7 +11,9 @@ block('footer')(
           elemMods: { sw: 24, lw: 6 },
           content: [
             { block: 'footer', elem: 'header', content: 'ORGANIZATION' },
-            { block: 'footer', elem: 'link', url: base + 'contributors', content: 'Contributors' }
+            { block: 'footer', elem: 'link', url: base + 'contributors', content: 'Contributors' },
+            { block: 'footer', elem: 'link', url: 'http://www.webpackbin.com', content: 'www.webpackbin.com' },
+            { block: 'footer', elem: 'link', url: 'http://www.christianalfoni.com', content: 'www.christianalfoni.com' }
           ]
         },
         {
@@ -19,7 +21,9 @@ block('footer')(
           elem: 'col',
           elemMods: { sw: 24, lw: 6, lo: 3 },
           content: [
-            { block: 'footer', elem: 'header', content: 'ARTICLES AND USEFUL LINKS' }
+            { block: 'footer', elem: 'header', content: 'ARTICLES AND USEFUL LINKS' },
+            { block: 'footer', elem: 'link', url: 'http://medium.com/p/5793c08db2cc', content: 'The story of Cerebral' },
+            { block: 'footer', elem: 'link', url: 'https://gist.github.com/christianalfoni/b08a99faa09df054afe87528a2134730', content: 'An unlikely success story' }
           ]
         },
         {
@@ -27,7 +31,9 @@ block('footer')(
           elem: 'col',
           elemMods: { sw: 24, lw: 6, lo: 6 },
           content: [
-            { block: 'footer', elem: 'header', content: 'HELP' }
+            { block: 'footer', elem: 'header', content: 'HELP' },
+            { block: 'footer', elem: 'link', url: 'https://github.com/cerebral/cerebral', content: 'Cerebral github repo' },
+            { block: 'footer', elem: 'link', url: 'https://discord.gg/0kIweV4bd2bwwsvH', content: 'Discord chat' }
           ]
         }
       ]
