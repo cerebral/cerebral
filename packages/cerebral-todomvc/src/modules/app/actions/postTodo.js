5eeea486325f881b2b800a59fffe7831faa1f138
Henri Hulski <henri.hulski@gazeta.pl>
2016-10-23 22:31:41 +0200

    fix(todomvc): fix the timeout when posting Todo

--- packages/cerebral-todomvc/src/modules/app/actions/postTodo.js
+++ packages/cerebral-todomvc/src/modules/app/actions/postTodo.js
@@ -1,14 +1,13 @@
 function postTodo ({path}) {
   // Simulating posting the todo.data and get an ID from
   // the server. We resolve with the new id
-  return new Promise(resolve => {
-    setTimeout(
-      resolve(path.success({
-        id: Date.now() + parseInt(Math.random() * 1000, 10)
-      }))
-    , 2000)
-      // Or error
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000)
   })
+  .then(() => path.success({
+    id: Date.now() + parseInt(Math.random() * 1000, 10)
+  }))
+  // Or error
 }
 
 export default postTodo
