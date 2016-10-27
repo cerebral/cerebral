37d8f9a581aa4dbad7e0ecb286f86be5be334816
Fops <fopsdev@gmail.com>
2016-10-27 22:25:59 +0200

    added provider and advanced async version 0.5

--- packages/cerebral-website-tutorial/src/components/App/index.js
+++ packages/cerebral-website-tutorial/src/components/App/index.js
@@ -3,16 +3,20 @@ import { connect } from 'cerebral/react'
 import HeaderButton from '../HeaderButton'
 import Toast from '../Toast'
 import Input from '../Input'
+import LoadDataButton from '../LoadDataButton'
 
 export default connect({
   appTitle: 'appTitle'
 }, {
-}, function App (props) {
+}, function App(props) {
   return (
     <div className='o-container o-container--medium'>
       <h1 className='u-high'>{ props.appTitle }</h1>
       <HeaderButton />
+      <br/>
       <Input />
+      <br/>
+      <LoadDataButton/>
       <Toast />
     </div>
   )
--- packages/cerebral-website-tutorial/src/index.js
+++ packages/cerebral-website-tutorial/src/index.js
@@ -16,7 +16,8 @@ const controller = Controller({
       message: 'no message yet'
     },
     originalValue: '',
-    extendedValue: ''
+    extendedValue: '',
+    data: {}
   },
   signals: {
     buttonClicked: [
@@ -28,40 +29,72 @@ const controller = Controller({
       myAction2,
       myAction3,
       ...showToast()
+    ],
+    loadDataClicked: [
+      ...showToast('Loading Data...', 1000),
+      GetData,
+      set(state`data`, input`result`),
+      ...showToast('How cool is that. @{data.name} has @{data.stargazers_count} stars!', 5000)
     ]
-  }
+
+  },
+  providers: [
+    HttpProvider({
+      baseUrl: 'https://api.github.com'
+    })
+  ],
 })
 
-function myAction1 ({input}) {
+function myAction1({input}) {
   input.value += ' extended by myAction1'
 }
 
-function myAction2 ({input, state}) {
+function myAction2({input, state}) {
   input.value += ' and also by myAction2'
   return ({
     aKeyAddedByMyAction2: 'testvalue'
   })
 }
 
-function myAction3 ({input, state}) {
+function myAction3({input, state}) {
   input.value = input.value.toUpperCase()
   input.message = input.value
   state.set('extendedValue', input.value)
 }
 
-function showToast (message, milliseconds) {
-  function action ({input, state, path}) {
+
+function GetData({input, state, http}) {
+  return http.get("/repos/cerebral/cerebral")
+    .then(response => ({
+      result: response.result
+    }))
+}
+
+
+function showToast(message, milliseconds) {
+  function action({input, state, path}) {
     let msg = message
     let ms = milliseconds
     if (!msg && input) {
       msg = input.message
     }
+
+    // replace the @{...} matches with current state value
+    let reg = new RegExp(/@{.*?}/g)
+    var matches = msg.match(reg)
+    if (matches) {
+      matches.forEach(m => {
+        let cleanedPath = m.replace("@{", "").replace("}", "")
+        msg = msg.replace(m, state.get(cleanedPath))
+      })
+    }
+
     if (!ms) {
       ms = 8000
     }
     state.set('toast.message', msg)
-    return new Promise(function (resolve, reject) {
-      window.setTimeout(function () {
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(function() {
         resolve({})
       }, ms)
     })
@@ -72,7 +105,7 @@ function showToast (message, milliseconds) {
   ]
 }
 render((
-  <Container controller={controller}>
+  <Container controller={ controller }>
     <App />
   </Container>
   ), document.querySelector('#root'))
