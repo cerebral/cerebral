abbe84128743f4f6f40de03d078748d10ef79c55
Christian Alfoni <christianalfoni@gmail.com>
2016-11-08 22:10:45 +0100

    feat(Devtools): promote devtools when not in production and no devtools

--- packages/cerebral/src/Controller.js
+++ packages/cerebral/src/Controller.js
@@ -59,6 +59,8 @@ class Controller extends EventEmitter {
 
     if (this.devtools) {
       this.devtools.init(this)
+    } else if (process.env.NODE_ENV !== 'production') {
+      console.warn('You are not using the Cerebral devtools. It is highly recommended to use it in combination with the debugger: https://cerebral.github.io/cerebral-website/getting-real/03_devtools.html')
     }
 
     if (this.router) this.router.init()
