44eb147fae4c88b21a5f48d4bceb353ac43b1325
Christian Alfoni <christianalfoni@gmail.com>
2016-11-09 14:23:56 +0100

    feat(Operators): allow operator template literals to use computed

--- packages/cerebral/src/operators/helpers/populatePath.js
+++ packages/cerebral/src/operators/helpers/populatePath.js
@@ -1,7 +1,10 @@
 export default function populatePath (context, strings, values) {
   return strings.reduce((currentPath, string, idx) => {
     const valueTemplate = values[idx]
-    const value = valueTemplate ? valueTemplate(context).toValue() : ''
-    return currentPath + string + value
+    if (valueTemplate && valueTemplate.getValue) {
+      return currentPath + string + context.state.compute(valueTemplate)
+    }
+
+    return currentPath + string + (valueTemplate ? valueTemplate(context).toValue() : '')
   }, '')
 }
