356413d04104cf1d11c939323588a85b16cd64bb
Christian Alfoni <christianalfoni@gmail.com>
2016-11-09 15:13:39 +0100

    feat(Operators): allow operator template literals to use computed (#406)

--- packages/cerebral/src/operators/string.test.js
+++ packages/cerebral/src/operators/string.test.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 import Controller from '../Controller'
 import assert from 'assert'
+import {Computed} from '../'
 import {input, string} from './'
 
 describe('operator.string', () => {
@@ -10,6 +11,24 @@ describe('operator.string', () => {
         test: [
           (context) => {
             assert.deepEqual(string`foo.${input`ref`}`(context).toValue(), 'foo.bar')
+          }
+        ]
+      }
+    })
+    controller.getSignal('test')({ref: 'bar'})
+  })
+  it('should allow computed in template literal', () => {
+    const computed = Computed({
+      foo: 'foo'
+    }, (props) => props.foo)
+    const controller = new Controller({
+      state: {
+        foo: 'bar'
+      },
+      signals: {
+        test: [
+          (context) => {
+            assert.deepEqual(string`foo.${computed}`(context).toValue(), 'foo.bar')
           }
         ]
       }
