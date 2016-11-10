68ce0b8ededbbe8f56db06b2f4ec97e2bd8b74f2
Henri Hulski <henri.hulski@gazeta.pl>
2016-11-10 13:42:50 +0100

    fix: replace node EventEmitter with EventEmitter3 (#407)
    ISSUES CLOSED: #389

--- packages/function-tree/src/index.js
+++ packages/function-tree/src/index.js
@@ -1,4 +1,4 @@
-import EventEmitter from 'events'
+import EventEmitter from 'eventemitter3'
 import executeTree from './executeTree'
 import createStaticTree from './staticTree'
 import ExecutionProvider from './providers/Execution'
