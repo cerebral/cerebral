297e91b2ab29445549d656a6fb5ef60d112ae46e
Mathias Nilsson <wicket.programmer@gmail.com>
2016-11-11 16:39:09 +0100

    fix(forms): fix circular dependsOn (#404)

--- packages/cerebral-forms/src/field.test.js
+++ packages/cerebral-forms/src/field.test.js
@@ -2,6 +2,7 @@
 import {Controller} from 'cerebral'
 import {form} from '.'
 import assert from 'assert'
+import changeField from './chains/changeField'
 
 describe('field', () => {
   it('should create new field by merging form', () => {
@@ -53,5 +54,47 @@ describe('field', () => {
         }
       }
     })
+  })
+
+  it('should update dependsOn', () => {
+    const controller = Controller({
+      state: {
+        form: form({
+          password: {
+            value: '',
+            isRequired: true,
+            requiredMessage: 'Password is required.',
+            validationRules: [
+              'minLength:15'
+            ],
+            errorMessages: [
+              'Password must be at least 15 characters.'
+            ],
+            dependsOn: 'form.repeatPassword'
+          },
+          repeatPassword: {
+            value: '',
+            isRequired: true,
+            validationRules: [
+              'equalsField:password'
+            ],
+            errorMessages: [
+              'Password must match repeat password.'
+            ],
+            requiredMessage: 'Repeat password is required.',
+            dependsOn: 'form.password'
+          }
+        })
+      },
+      signals: {
+        fieldChanged: changeField
+      }
+    })
+    controller.getSignal('fieldChanged')({field: 'form.password', value: 'abcdefghijklmnopqrstuvxyz'})
+    assert.equal(controller.getState('form.password.isValid'), false)
+    assert.equal(controller.getState('form.repeatPassword.isValid'), false)
+    controller.getSignal('fieldChanged')({field: 'form.repeatPassword', value: 'abcdefghijklmnopqrstuvxyz'})
+    assert.equal(controller.getState('form.password.isValid'), true)
+    assert.equal(controller.getState('form.repeatPassword.isValid'), true)
   })
 })
