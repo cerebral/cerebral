2ff680b5eed5412598af87d4c100b7a6ff11b9cb
Julio Makdisse Saito <saitodisse@gmail.com>
2016-11-15 20:11:38 -0200

    feat(demos): firebaseGetValue get all clients for current user

--- packages/demo/src/factories/firebaseGetValue.js
+++ packages/demo/src/factories/firebaseGetValue.js
@@ -1,7 +1,16 @@
-function firebaseGetValue (collectionName) {
-  return function ({ firebase, path }) {
-    return firebase.value(collectionName)
-      .then(path.success)
+function firebaseGetValue ({firebasePath, uidPath, localCollectionPath}) {
+  return function ({firebase, state, path}) {
+    let uid = ''
+    if (uidPath) {
+      // firebase collection is under an user_id
+      uid = `.${state.get(uidPath)}`
+    }
+
+    return firebase.value(`${firebasePath}${uid}`)
+      .then((result) => {
+        state.set(localCollectionPath, result.value)
+        return path.success(result)
+      })
       .catch(path.error)
   }
 }
--- packages/demo/src/modules/app/signals/firebaseInit.js
+++ packages/demo/src/modules/app/signals/firebaseInit.js
@@ -1,11 +1,18 @@
 import firebaseListen from '../../../factories/firebaseListen'
-// import firebaseGetValue from '../../../factories/firebaseGetValue'
+import firebaseGetValue from '../../../factories/firebaseGetValue'
 
 export default [
   firebaseListen({
     moduleName: 'clients',
     firebasePath: 'clients',
     uidPath: 'user.currentUser.uid'
-  })
-  // firebaseGetValue('clients', 'user.currentUser.uid', 'clients.all')
+  }),
+  firebaseGetValue({
+    firebasePath: 'clients',
+    uidPath: 'user.currentUser.uid',
+    localCollectionPath: 'clients.all'
+  }), {
+    success: [],
+    error: []
+  }
 ]
