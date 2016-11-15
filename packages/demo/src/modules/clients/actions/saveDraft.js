301b15256fb52d94171c21360e6e56b9d7b88021
Julio Makdisse Saito <saitodisse@gmail.com>
2016-11-15 02:03:34 -0200

    feat(demos): add/update client

--- packages/demo/src/modules/clients/actions/saveDraft.js
+++ packages/demo/src/modules/clients/actions/saveDraft.js
@@ -1,9 +1,13 @@
-function saveDraft ({state, path}) {
+function saveDraft ({state, path, firebase}) {
   const draft = state.get('clients.$draft')
+  const uid = state.get('user.currentUser.uid')
 
-  state.set(`clients.all.${draft.ref}`, draft)
-
-  return path.success()
+  firebase.update(`clients.${uid}.${draft.ref}`, draft)
+    .then(() => {
+      state.set(`clients.all.${draft.ref}`, draft)
+    })
+    .then(path.success)
+    .catch(path.error)
 }
 
 export default saveDraft
