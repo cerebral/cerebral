e65b502ea774ea3f8f56677ca56444c994de41b5
Yusuf Safak Bayram <yusufsafak@yahoo.com>
2016-11-24 22:10:07 +0100

    feat(form): add initializeForm factory

---
+++ packages/cerebral-forms/src/factories/initalizeForm.test.js
@@ -0,0 +1,277 @@
+/* eslint-env mocha */
+import {Controller} from 'cerebral'
+import {input} from 'cerebral/operators'
+import {form, initializeForm, changeField} from '..'
+import assert from 'assert'
+
+describe('initializeForm', () => {
+  it('should initialize form with data', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm('form', {
+            name: 'Ben'
+          })
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          }
+        })
+      }
+    })
+    controller.getSignal('initialize')()
+    assert.equal(controller.getState('form.name.value'), 'Ben')
+  })
+  it('should initialize form by input tag', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`, input`initialValues`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          }
+        })
+      }
+    })
+    controller.getSignal('initialize')({
+      form: 'form',
+      initialValues: {
+        name: 'Ben'
+      }
+    })
+    assert.equal(controller.getState('form.name.value'), 'Ben')
+  })
+  it('should change defaultValue by input tag', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`, input`initialValues`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: '',
+            defaultValue: 'Tom'
+          }
+        })
+      }
+    })
+    controller.getSignal('initialize')({
+      form: 'form',
+      initialValues: {
+        name: 'Ben'
+      }
+    })
+    assert.equal(controller.getState('form.name.defaultValue'), 'Ben')
+  })
+  it('initialize without initialValues should behave like resetForm', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          }
+        })
+      }
+    })
+    controller.getSignal('changeField')({field: 'form.name', value: 'Other name'})
+    controller.getSignal('initialize')({
+      form: 'form'
+    })
+    assert.equal(controller.getState('form.name.value'), '')
+  })
+  it('Should support nested forms', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`, input`initialValues`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          },
+          address: form({
+            street: {
+              value: ''
+            }
+          })
+        })
+      }
+    })
+    controller.getSignal('initialize')({
+      form: 'form',
+      initialValues: {
+        name: 'Ben',
+        address: {
+          street: '21 2nd Street'
+        }
+      }
+    })
+    assert.equal(controller.getState('form.name.value'), 'Ben')
+    assert.equal(controller.getState('form.address.street.value'), '21 2nd Street')
+  })
+  it('Should reset nested Form', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          },
+          address: form({
+            street: {
+              value: ''
+            }
+          })
+        })
+      }
+    })
+    controller.getSignal('changeField')({field: 'form.address.street', value: '21 2nd Street'})
+    controller.getSignal('initialize')({
+      form: 'form'
+    })
+    assert.equal(controller.getState('form.address.street.value'), '')
+  })
+  it('Should support nested form arrays', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`, input`initialValues`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          },
+          address: [
+            form({
+              street: {
+                value: ''
+              }
+            })
+          ]
+        })
+      }
+    })
+
+    controller.getSignal('initialize')({
+      form: 'form',
+      initialValues: {
+        name: 'Ben',
+        address: [
+          { street: '21 2nd Street' },
+          { street: '31 3nd Street' }
+        ]
+      }
+    })
+    assert.equal(controller.getState('form.name.value'), 'Ben')
+    assert.equal(controller.getState('form.address.0.street.value'), '21 2nd Street')
+    assert.equal(controller.getState('form.address.1.street.value'), '31 3nd Street')
+  })
+  it('Should reset nested form arrays', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`, input`initialValues`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          },
+          address: [
+            form({
+              street: {
+                value: ''
+              }
+            }),
+            form({
+              street: {
+                value: '31 3nd Street'
+              }
+            })
+          ]
+        })
+      }
+    })
+    controller.getSignal('initialize')({
+      form: 'form',
+      initialValues: {
+        name: 'Ben',
+        address: [
+          { street: '21 2nd Street' }
+        ]
+      }
+    })
+    assert.equal(controller.getState('form.name.value'), 'Ben')
+    assert.equal(controller.getState('form.address.0.street.value'), '21 2nd Street')
+    assert.equal(controller.getState('form.address.1.street.value'), '31 3nd Street')
+  })
+  it('Reset nested form with arrays', () => {
+    const controller = Controller({
+      signals: {
+        initialize: [
+          initializeForm(input`form`, input`initialValues`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: ''
+          },
+          address: [
+            form({
+              street: {
+                value: ''
+              }
+            }),
+            form({
+              street: {
+                value: '31 3nd Street'
+              }
+            })
+          ]
+        })
+      }
+    })
+    controller.getSignal('changeField')({field: 'form.address.0.street', value: '21 2nd Street'})
+    controller.getSignal('changeField')({field: 'form.address.1.street', value: ''})
+    controller.getSignal('changeField')({field: 'form.name', value: 'Ben'})
+    controller.getSignal('initialize')({
+      form: 'form'
+    })
+    assert.equal(controller.getState('form.name.value'), '')
+    assert.equal(controller.getState('form.address.0.street.value'), '')
+    assert.equal(controller.getState('form.address.1.street.value'), '31 3nd Street')
+  })
+})
---
+++ packages/cerebral-forms/src/factories/initializeForm.js
@@ -0,0 +1,48 @@
+import configureField from '../utils/configureField'
+
+function initializeObject (form, initialValues) {
+  return Object.keys(form).reduce(function (newForm, key) {
+    if (form[key] === Object(form[key])) {
+      if (Array.isArray(form[key])) {
+        newForm[key] = initializeArray(form[key], initialValues && initialValues[key])
+      } else if ('value' in form[key]) {
+        const newField = Object.keys(form[key]).reduce((newField, fKey) => {
+          newField[fKey] = form[key][fKey]
+          return newField
+        }, {})
+        newField.defaultValue = initialValues && initialValues[key] || newField.defaultValue
+        newField.value = newField.defaultValue
+        newForm[key] = configureField(form, newField)
+      } else {
+        newForm[key] = initializeObject(form[key], initialValues && initialValues[key])
+      }
+    }
+
+    return newForm
+  }, {})
+}
+
+function initializeArray (formArray, initialValues) {
+  if(initialValues && formArray.length < initialValues.length){
+    return initialValues.reduce((newFormArray, value, index) => {
+      newFormArray[index] = initializeObject(formArray[index] || formArray[0], value)
+      return newFormArray
+    }, [])
+  }
+  return formArray.reduce((newFormArray, form, index) => {
+      newFormArray[index] = initializeObject(form, initialValues && initialValues[index])
+      return newFormArray
+    }, [])
+}
+
+export default function initializeFormFactory (formPathTemplate, initialValuesTemplate) {
+  function initializeForm (context) {
+    const formPath = typeof formPathTemplate === 'function' ? formPathTemplate(context).value : formPathTemplate
+    const initialValues = typeof initialValuesTemplate === 'function' ? initialValuesTemplate(context).value : initialValuesTemplate
+    const form = context.state.get(formPath)
+
+    context.state.set(formPath, initializeObject(form, initialValues))
+  }
+
+  return initializeForm
+}
