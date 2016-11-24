3d7167f31a102e9a104b0c188b6f6d986e6fbbaa
Gaspard Bucher <gaspard@lucidogen.io>
2016-11-24 15:34:39 +0100

    feat(Operators): add support for multiple arguments in "when"

--- packages/cerebral/src/operators/when.js
+++ packages/cerebral/src/operators/when.js
@@ -1,7 +1,9 @@
-function whenFactory (target, whenFunc) {
+function whenFactory (...args) {
+  const whenFunc = args.length > 1 ? args[args.length - 1] : null
+  const valueTemplates = args.length > 1 ? args.slice(0, -1) : args
   function when (context) {
-    const targetValue = target(context).value
-    const isTrue = Boolean(typeof whenFunc === 'function' ? whenFunc(targetValue) : targetValue)
+    const values = valueTemplates.map(value => typeof value === 'function' ? value(context).value : value)
+    const isTrue = Boolean(whenFunc ? whenFunc(...values) : values[0])
 
     return isTrue ? context.path.true() : context.path.false()
   }
