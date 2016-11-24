2019f3a94bca01e351a6cd666d7abd49a4201657
Yusuf Safak Bayram <yusufsafak@yahoo.com>
2016-11-24 22:36:01 +0100

    test(form): add more validateForm tests

--- packages/cerebral-forms/src/factories/validateForm.test.js
+++ packages/cerebral-forms/src/factories/validateForm.test.js
@@ -51,4 +51,81 @@ describe('validateField', () => {
     })
     assert.equal(controller.getState('form.name.isValid'), true)
   })
+  it('should validate nested forms', () => {
+    const controller = Controller({
+      signals: {
+        validateForm: [
+          validateForm(input`form`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: 'Ben',
+            validationRules: ['minLength:4']
+          },
+          address: form({
+            street: {
+              value: '21',
+              validationRules: ['minLength:4']
+            }
+          })
+        })
+      }
+    })
+    assert.equal(controller.getState('form.name.isValid'), false)
+    assert.equal(controller.getState('form.address.street.isValid'), false)
+    controller.getSignal('changeField')({field: 'form.name', value: 'Longer name'})
+    controller.getSignal('changeField')({field: 'form.address.street', value: '21 2nd Street'})
+    controller.getSignal('validateForm')({
+      form: 'form'
+    })
+    assert.equal(controller.getState('form.name.isValid'), true)
+    assert.equal(controller.getState('form.address.street.isValid'), true)
+  })
+  it('should validate nested form arrays', () => {
+    const controller = Controller({
+      signals: {
+        validateForm: [
+          validateForm(input`form`)
+        ],
+        changeField
+      },
+      state: {
+        form: form({
+          name: {
+            value: 'Ben',
+            validationRules: ['minLength:4']
+          },
+          address: [
+            form({
+              street: {
+                value: '21',
+                validationRules: ['minLength:4']
+              }
+            }),
+            form({
+              street: {
+                value: '31 3nd Street',
+                validationRules: ['minLength:4']
+              }
+            })
+          ]
+
+        })
+      }
+    })
+    assert.equal(controller.getState('form.name.isValid'), false)
+    assert.equal(controller.getState('form.address.0.street.isValid'), false)
+    assert.equal(controller.getState('form.address.1.street.isValid'), true)
+    controller.getSignal('changeField')({field: 'form.name', value: 'Longer name'})
+    controller.getSignal('changeField')({field: 'form.address.0.street', value: '21 2nd Street'})
+    controller.getSignal('validateForm')({
+      form: 'form'
+    })
+    assert.equal(controller.getState('form.name.isValid'), true)
+    assert.equal(controller.getState('form.address.0.street.isValid'), true)
+    assert.equal(controller.getState('form.address.1.street.isValid'), true)
+  })
 })
