e514028a2a86bfe66329a6c76b9218c5756c9911
yusufsafak <yusufsafak@yahoo.com>
2016-11-30 22:34:05 +0100

    feat(forms): remove initializeForm (#521)

--- packages/cerebral-forms/src/index.js
+++ packages/cerebral-forms/src/index.js
@@ -9,7 +9,6 @@ export {default as changeField} from './chains/changeField'
 export {default as validateField} from './factories/validateField'
 export {default as validateForm} from './factories/validateForm'
 export {default as resetForm} from './factories/resetForm'
-export {default as initializeForm} from './factories/initializeForm'
 
 export {default as formToJSON} from './helpers/formToJSON'
 export {default as getFormFields} from './helpers/getFormFields'
