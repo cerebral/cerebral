2c19161e6960d15f5bd0c5bbd8dfe36724c3d496
Gaspard Bucher <gaspard@lucidogen.io>
2016-12-17 15:05:02 +0100

    fix(firebase): fix code to not use buggy 'catch' from firebase promises

--- packages/cerebral-provider-firebase/src/signInWithEmailAndPassword.js
+++ packages/cerebral-provider-firebase/src/signInWithEmailAndPassword.js
@@ -3,22 +3,24 @@ import {createUser} from './helpers'
 
 export default function signInWithEmailAndPassword (email, password) {
   return firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => {
-      return new Promise((resolve) => {
-        const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-          unsubscribe()
-          resolve({
-            user: createUser(user)
+    .then(
+      () => {
+        return new Promise((resolve) => {
+          const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+            unsubscribe()
+            resolve({
+              user: createUser(user)
+            })
           })
         })
-      })
-    })
-    .catch((error) => {
-      return {
-        error: {
-          code: error.code,
-          message: error.message
+      },
+      (error) => {
+        return {
+          error: {
+            code: error.code,
+            message: error.message
+          }
         }
       }
-    })
+    )
 }
