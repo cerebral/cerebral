db69b75cbfc9a8bc9ec40a4751ebcfa3e843ed70
fops <fopsdev@gmail.com>
2017-01-16 08:27:26 +0100

    fix(useragent): module path is now string (#602)
    module path is now string

--- packages/cerebral-module-useragent/src/signalFactories/windowChanged.js
+++ packages/cerebral-module-useragent/src/signalFactories/windowChanged.js
@@ -2,7 +2,7 @@ import {merge, state, input} from 'cerebral/operators'
 
 export default (path) => {
   return [
-    merge(state`${path.join('.')}.media`, input`media`),
-    merge(state`${path.join('.')}.window`, input`windowSpec`)
+    merge(state`${path}.media`, input`media`),
+    merge(state`${path}.window`, input`windowSpec`)
   ]
 }
