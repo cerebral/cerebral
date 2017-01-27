ec57f19c89353a48c8ec9d064f58945f4516cee3
Christian Alfoni <christianalfoni@gmail.com>
2017-01-27 11:42:46 +0100

    fix(firebase): handle signOut and redirect result correctly (#628)

--- packages/cerebral-provider-firebase/src/signOut.js
+++ packages/cerebral-provider-firebase/src/signOut.js
@@ -2,4 +2,11 @@ import firebase from 'firebase'
 
 export default function signOut () {
   return firebase.auth().signOut()
+    .then(() => {
+      for (let key in window.localStorage) {
+        if (key.indexOf('firebase:authUser') === 0) {
+          window.localStorage.removeItem(key)
+        }
+      }
+    })
 }
