0f115f41e99dcc2924b921dba15622694f7cfecb
Julio Makdisse Saito <saitodisse@gmail.com>
2017-02-06 11:38:49 -0200

    docs(tutorial): tutorial script will not remove unnecessary files (#657)

--- docs/tutorial/scripts/prepare.js
+++ docs/tutorial/scripts/prepare.js
@@ -1,8 +1,9 @@
-/* global rm, mkdir, ls, cp, sed */
+/* global rm, mkdir, ls, cp, sed, find */
 var path = require('path')
 var paths = require('./paths')
 
 function prepare (part) {
+  // create a new src folder if does not exist
   var hasSrc = ls(paths.root).reduce(function (currentHasSrc, file) {
     if (file === 'src') {
       return true
@@ -11,16 +12,45 @@ function prepare (part) {
     return currentHasSrc
   }, false)
 
-  if (hasSrc) {
-    ls(paths.src).forEach(function (file) {
-      if (file !== 'index.js') {
-        rm('-rf', path.join(paths.src, file))
-      }
-    })
-  } else {
+  if (!hasSrc) {
     mkdir(paths.src)
   }
 
+  // remove only source files that do not exist on "parts" folder
+  let partsFiles = []
+  const partPath = path.join(paths.parts, part, 'src').replace(/\\/g, '/')
+  find(partPath).forEach(function (file) {
+    const filePathPartial = file.replace(partPath, '')
+    if (filePathPartial.length > 0) {
+      partsFiles.push({
+        partialPath: filePathPartial,
+        fullPath: file
+      })
+    }
+  })
+
+  let srcFiles = []
+  const sourcePath = paths.src.replace(/\\/g, '/')
+  find(sourcePath).forEach(function (file) {
+    const filePathPartial = file.replace(sourcePath, '')
+    if (filePathPartial.length > 0) {
+      srcFiles.push({
+        partialPath: filePathPartial,
+        fullPath: file
+      })
+    }
+  })
+
+  const strangeFilesToDelete = srcFiles.filter((srcFileObj) => {
+    const srcFileExists = partsFiles.find(partFile => partFile.partialPath === srcFileObj.partialPath)
+    return !srcFileExists
+  })
+
+  strangeFilesToDelete.forEach(function (srcFileObjToDelete) {
+    rm('-rf', srcFileObjToDelete.fullPath)
+  })
+
+  // copy files from parts folder
   ls(path.join(paths.parts, part, 'src')).forEach(function (file) {
     cp('-R', path.join(paths.parts, part, 'src', file), paths.src)
   })
--- docs/tutorial/src/modules/app/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  state: {
-    title: 'Hello from Cerebral!',
-    subTitle: 'Working on my state management',
-    toast: null,
-    activeTab: 'home'
-  }
-}
