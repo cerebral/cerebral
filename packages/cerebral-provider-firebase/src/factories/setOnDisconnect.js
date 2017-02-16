e1f76516d52d92f0fc50cc5eb55b8b2f4d40a8b3
Christian Alfoni <christianalfoni@gmail.com>
2017-02-16 10:35:34 +0100

    feat(firebase): add onDisconnect features

---
+++ packages/cerebral-provider-firebase/src/factories/setOnDisconnect.js
@@ -0,0 +1,7 @@
+export default function setOnDisconnectFactory (setPath, value) {
+  function setOnDisconnect ({firebase, resolve}) {
+    firebase.setOnDisconnect(resolve.value(setPath), resolve.value(value))
+  }
+
+  return setOnDisconnect
+}
