d8335fdcb35727f23bd0a09142e6479ea1d5ab4f
Christian Alfoni <christianalfoni@gmail.com>
2017-02-19 23:53:03 +0100

    docs(website): clean up UI and fixed styling issues

---
+++ docs/docs/docs/get_started/demos.md
@@ -0,0 +1,13 @@
+# Demos
+
+## Webpackbin
+The webpack bin service is built with Cerebral.
+
+[Open Webpackbin](https://www.webpackbin.com)
+
+## TodoMVC
+The traditional web application for showing off the typical challenges of building apps. Handle state, deriving state and structure the application.
+
+[Open TodoMVC demo](/todomvc)
+
+[Go to repo](https://github.com/cerebral/cerebral/tree/master/demos/todomvc)
--- docs/docs/pages/docs/index.js
+++ docs/docs/pages/docs/index.js
@@ -3,7 +3,6 @@ import Navigation from './navigation'
 import NavigationMobile from './navigationMobile'
 import TOC from './toc'
 import Doc from './doc'
-import Footer from './footer'
 
 function Docs (props) {
   const doc = props.docs[props.sectionName][props.docName]
@@ -16,7 +15,6 @@ function Docs (props) {
         <TOC docName={props.docName} sectionName={props.sectionName} sections={props.docs[props.sectionName]} />
         <Doc doc={doc.tree} docName={props.docName} sectionName={props.sectionName} />
       </div>
-      <Footer />
       <div style={{zIndex: 99, backgroundColor: '#DD4A68', color: '#fff', padding: '5px 10px', position: 'fixed', width: '100%', bottom: 0, left: 0}}>
         Docs are still being written and might not be up to date with API
       </div>
