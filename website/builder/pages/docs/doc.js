d93b07d820de330f73892fa9e93415251873de97
Michael Plotke <michael.plotke@perkinelmer.com>
2017-03-20 19:52:22 -0400

    fix(website): correct various layout bugs
    These changes have been tested on every major browser, including IE and Safari. They seem okay.

--- website/builder/pages/docs/doc.js
+++ website/builder/pages/docs/doc.js
@@ -3,10 +3,12 @@ import React from 'react'
 function Doc (props) {
   return (
     <div className='docs-doc'>
-      <div className='docs-doc-edit'>
-        <a href={`https://github.com/cerebral/cerebral/tree/master/docs/${props.sectionName}/${props.docName}.md`} target='_new'>Edit on Github</a>
+      <div className='docs-doc-content'>
+        <div className='docs-doc-edit'>
+          <a href={`https://github.com/cerebral/cerebral/tree/master/docs/${props.sectionName}/${props.docName}.md`} target='_new'>Edit on Github</a>
+        </div>
+        {props.doc}
       </div>
-      {props.doc}
     </div>
   )
 }
