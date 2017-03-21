4d7608f2d0831ae91d4e7d68ca38801f79d7f4b6
Michael Plotke <michael.plotke@perkinelmer.com>
2017-03-21 17:28:41 -0400

    fix(website): improve hamburger menu icon appearance and compatibility

--- website/builder/pages/docs/navigationMobile.js
+++ website/builder/pages/docs/navigationMobile.js
@@ -19,9 +19,7 @@ function Navigation (props) {
 
   return (
     <div id='navigation-mobile' className='docs-navigation mobile'>
-      <div id='hamburger' className='docs-navigation-hamburger'>
-        &#9776;
-      </div>
+      <div id='hamburger' className='docs-navigation-hamburger'/>
       <div className='docs-navigation-title mobile'>
         {props.sectionName.replace('_', ' ')}
       </div>
