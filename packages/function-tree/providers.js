619dc5f558a71e7ef63ff29e94f4de9a9bc4a73c
Christian Alfoni <christianalfoni@gmail.com>
2017-04-04 22:48:42 +0200

    fix(function-tree): remove redux provider (#815)

--- packages/function-tree/providers.js
+++ packages/function-tree/providers.js
@@ -2,4 +2,3 @@ exports.ContextProvider = require('./lib/providers/Context').default
 exports.ExecutionProvider = require('./lib/providers/Execution').default
 exports.PropsProvider = require('./lib/providers/Props').default
 exports.PathProvider = require('./lib/providers/Path').default
-exports.ReduxProvider = require('./lib/providers/Redux').default
