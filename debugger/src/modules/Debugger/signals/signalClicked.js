6399ad3f9cd6d53f4ea705537e35f143e9f68580
Christian Alfoni <christianalfoni@gmail.com>
2017-04-12 09:35:33 +0200

    feat(debugger): make general improvements to debugger (#839)

--- debugger/src/modules/Debugger/signals/signalClicked.js
+++ debugger/src/modules/Debugger/signals/signalClicked.js
@@ -1,5 +1,8 @@
+import {set} from 'cerebral/operators'
+import {state} from 'cerebral/tags'
 import setCurrentExecutionId from '../actions/setCurrentExecutionId'
 
 export default [
+  set(state`debugger.currentPage`, 'signals'),
   setCurrentExecutionId
 ]
