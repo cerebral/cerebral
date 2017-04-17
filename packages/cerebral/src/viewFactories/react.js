96d5d27993a75c5b7ddb02945e3192b125fd4ae9
Filip Malinowski <crimhead@gmail.com>
2017-04-17 10:09:44 +0200

    fix(Views): updated PropTypes to come from separate package (#828)

--- packages/cerebral/src/viewFactories/react.js
+++ packages/cerebral/src/viewFactories/react.js
@@ -4,7 +4,24 @@ import StateContainerFactory from './StateContainer'
 import HocFactory from './Hoc'
 import connectFactory, { decoratorFactory } from './connect'
 
-export const Container = ContainerFactory(React)
-export const StateContainer = StateContainerFactory(React)
-export const connect = connectFactory(HocFactory(React))
-export const decorator = decoratorFactory(HocFactory(React))
+let PropTypes
+const [major, minor] = React.version.split('.')
+
+if ((major === '15' && minor >= 5) || major >= 16) {
+  try {
+    PropTypes = require('prop-types')
+  } catch (e) {
+    console.error(
+      'In order to support react@15.5+, you need to have prop-types package installed. ' +
+      'Please add prop-types to your dependencies (`npm install --save prop-types`)'
+    )
+    throw e
+  }
+}
+
+PropTypes = PropTypes || React.PropTypes
+
+export const Container = ContainerFactory(React, PropTypes)
+export const StateContainer = StateContainerFactory(React, PropTypes)
+export const connect = connectFactory(HocFactory(React, PropTypes))
+export const decorator = decoratorFactory(HocFactory(React, PropTypes))
