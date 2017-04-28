141dcc0151c6b338a98af4be2c44efdef7d252c3
Eli Yukelzon <eliy@codevalue.net>
2017-04-28 17:40:35 +0300

    test(shortcuts): added tests, bootstrap to travis

--- packages/cerebral-module-shortcuts/src/index.js
+++ packages/cerebral-module-shortcuts/src/index.js
@@ -4,10 +4,10 @@ export default function (shortcuts) {
   return module => {
     module.controller.on('initialized', () => {
       Object.keys(shortcuts).forEach(shortcut => {
-        const spaceShortcut = shortway(shortcut, () => {
+        const registeredShortcut = shortway(shortcut, () => {
           module.controller.getSignal(shortcuts[shortcut])({})
         })
-        document.addEventListener('keyup', spaceShortcut)
+        document.addEventListener('keyup', registeredShortcut)
       })
     })
 
