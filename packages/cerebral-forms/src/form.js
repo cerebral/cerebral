da1e1473a0cbe0788660ace30d05ce2531d14f2d
Henri Hulski <henri.hulski@gazeta.pl>
2017-05-06 09:53:38 +0200

    refactor(forms): remove depredicated cerebral-forms

--- packages/cerebral-forms/src/form.js
+++
@@ -1,9 +0,0 @@
-import configureField from './utils/configureField.js'
-
-export default function form (formData) {
-  return Object.keys(formData).reduce((form, key) => {
-    form[key] = configureField(formData, formData[key])
-
-    return form
-  }, {})
-}
