6acfba852bf186a001867c5f00b0083942da2809
Christian Alfoni <christianalfoni@gmail.com>
2017-08-07 13:35:58 +0200

    chore(release): add travis next release script

--- scripts/release/cooker.js
+++ scripts/release/cooker.js
@@ -3,6 +3,8 @@ import { Cooker } from 'repo-cooker'
 
 const dryRun = process.argv[2] !== '--publish'
 
+process.env.REPO_COOKER_GITHUB_TOKEN = process.env.GH_TOKEN
+
 export const cooker = Cooker({
   devtools: new Devtools({
     host: 'localhost:8787',
