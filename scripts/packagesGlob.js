5a13354a93b6001eaeaca86c569462bc3d5fd8be
Gaspard Bucher <gaspard@lucidogen.io>
2017-08-16 15:37:21 +0200

    chore(website): fix dependencies for website build on ci

--- scripts/packagesGlob.js
+++ scripts/packagesGlob.js
@@ -1,5 +1,6 @@
 module.exports = {
-  nodemos: 'packages/node_modules/{,@cerebral}/*/package.json',
-  all: 'packages/{demos,node_modules,node_modules/@cerebral}/*/package.json',
+  nodemos: 'packages/node_modules/{,@cerebral,website}/*/package.json',
+  all:
+    'packages/{demos,node_modules,node_modules/@cerebral,website}/*/package.json',
   demos: 'packages/demos/*/package.json',
 }
