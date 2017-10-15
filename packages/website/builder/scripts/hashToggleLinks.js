0b286e784b15fc9b49cba58cab12bd45f726430f
Christian Alfoni <christianalfoni@gmail.com>
2017-10-15 12:35:11 +0200

    fix(website): fix hash links toggle

--- packages/website/builder/scripts/hashToggleLinks.js
+++ packages/website/builder/scripts/hashToggleLinks.js
@@ -4,8 +4,8 @@ document.querySelector('#nav').addEventListener('click', event => {
   if (
     node.tagName === 'A' &&
     node.href.indexOf('#') >= 0 &&
-    node.parentNode.parentNode.childNodes[0].tagName === 'INPUT'
+    node.parentNode.childNodes[0].tagName === 'INPUT'
   ) {
-    node.parentNode.parentNode.childNodes[0].click()
+    node.parentNode.childNodes[0].click()
   }
 })
