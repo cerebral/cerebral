dd8c910318078f14b2d6cc6da039da18c8d434e9
Adam Schroder <aschroder@inlogik.com>
2018-01-27 20:40:06 +1100

    feat (proxy): infer more and show a possible way for no-parameter methods

--- packages/demos/todomvc-fluent/src/Hello.tsx
+++ packages/demos/todomvc-fluent/src/Hello.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import { connect } from '@cerebral/fluent';
-import { Connector } from './app';
+import { connect } from './app';
 
-export default connect(
-  ({ state, props }: Connector<{ baz: string }>) => ({
-    foo: state.foo
-  }),
-  function Hello ({ foo, baz }) {
-    return <h1>Hello there! {foo}</h1>;
-  }
-);
+export default connect<{ baz: string }>()
+  .with(({ state, props }) => ({
+      foo: state.foo
+  }))
+  .to(function Hello (context) {
+    return <h1>Hello there! {context.baz}</h1>;
+  });
