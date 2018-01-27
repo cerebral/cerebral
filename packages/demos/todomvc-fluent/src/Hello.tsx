37a8febd17fee2f0c2dcb1aef25e8c3e84c47113
Christian Alfoni <christianalfoni@gmail.com>
2018-01-27 02:24:14 +0100

    feat(fluent): connect and todomvc running

---
+++ packages/demos/todomvc-fluent/src/Hello.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { connect } from '@cerebral/fluent';
+import { Connector } from './app'
+
+export default connect(
+  ({ state, props }: Connector<{ baz: string }>) => ({
+    foo: state.foo
+  }),
+  function Hello ({ foo, baz }) {
+    return <h1>Hello there! {foo}</h1>
+  }
+);
