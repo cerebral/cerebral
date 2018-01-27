2e15fd22517725c631a92c18774e53d895d7a9dd
Christian Alfoni <christianalfoni@gmail.com>
2018-01-27 12:36:41 +0100

    refactor(fluent): refactor todomvc-fluent

--- packages/demos/todomvc-fluent/src/Hello.tsx
+++ packages/demos/todomvc-fluent/src/Hello.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import { connect } from './app';
 
 export default connect<{ baz: string }>()
-  .with(({ state, props }) => ({
-      foo: state.foo
+  .with(({ state, signals, props }) => ({
+      foo: state.foo,
+      runMe: signals.test
   }))
-  .to(function Hello (context) {
-    return <h1>Hello there! {context.baz}</h1>;
+  .to(function Hello ({ foo, baz, runMe }) {
+    return <h1>Hello there! {baz}</h1>;
   });
