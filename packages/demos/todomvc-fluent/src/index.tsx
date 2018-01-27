2c8fbb32f3dd5b1f428d8ac982d41e1662530928
Christian Alfoni <christianalfoni@gmail.com>
2018-01-27 02:50:53 +0100

    fix(fluent): make debugger work

--- packages/demos/todomvc-fluent/src/index.tsx
+++ packages/demos/todomvc-fluent/src/index.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import Devtools from 'cerebral/devtools';
 import { Controller, Container } from '@cerebral/fluent';
 import Hello from './Hello';
-import app from './app';
+import app, { State, Signals } from './app';
 
-const controller = Controller(app);
+const controller = Controller<State, Signals>(app, {
+  devtools: Devtools({
+    host: 'localhost:8686'
+  })
+});
+
+setTimeout(
+  () => {
+    controller.signals.test({});
+  },
+  1000
+);
 
 render(
   <Container controller={controller}>
