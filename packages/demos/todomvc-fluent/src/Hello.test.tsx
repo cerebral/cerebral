c39c038e6e9f66f084478ad6de175a62ce7a8db9
Christian Alfoni <christianalfoni@gmail.com>
2018-01-27 02:50:53 +0100

    fix(fluent): make debugger work

--- packages/demos/todomvc-fluent/src/Hello.test.tsx
+++ packages/demos/todomvc-fluent/src/Hello.test.tsx
@@ -4,5 +4,5 @@ import Hello from './Hello';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Hello />, div);
+  ReactDOM.render(<Hello baz="moh" />, div);
 });
