76079f601502e9a857da3c8ee13f3293bdc9bf4b
Christian Alfoni <christianalfoni@gmail.com>
2018-01-27 02:50:53 +0100

    fix(fluent): make debugger work

--- packages/demos/todomvc-fluent/src/app.ts
+++ packages/demos/todomvc-fluent/src/app.ts
@@ -1,20 +1,33 @@
-import { Module } from '@cerebral/fluent';
+import { Module, Sequence } from '@cerebral/fluent';
 
 export type State = {
   foo: string
 };
 
+export type Signals = {
+  test: (props: {}) => void
+};
+
 export type Context = {
   state: State
-}
+};
 
 export type Connector<Props = {}> = {
   state: State,
   props: Props
-}
+};
 
-export default Module<State, {}>({
+const testSequence = Sequence<Context, {}>(s => s
+  .action(function test ({ state }) {
+    state.foo = 'bar2';
+  })
+);
+
+export default Module<State, Signals>({
   state: {
     foo: 'bar'
+  },
+  signals: {
+    test: testSequence
   }
 });
