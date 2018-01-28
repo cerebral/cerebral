793b5b64a9a0ac74257f7d1d5a16ab0f2ad53d2a
Adam Schroder <aschroder@inlogik.com>
2018-01-28 15:20:01 +1100

    feat (proxy): found a way to infer type for modules

--- packages/demos/todomvc-fluent/src/Hello.tsx
+++ packages/demos/todomvc-fluent/src/Hello.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 import { connect } from './app';
 
-export default connect<{ baz: string }>()
+var result = connect<{ baz: string }>()
   .with(({ state, signals, props }) => ({
       foo: state.foo,
       runMe: signals.test
-  }))
-  .to(
-    class Hello extends React.Component {
-      render () {
-        return <h1>Hello </h1>
-      }
+  }));
+
+export default result.to(
+  class Hello extends React.Component<typeof result.type> {
+    render () {
+      return (
+        <div>
+          <h1>Hello {this.props.foo}</h1>
+          <button onClick={e => this.props.runMe()}>Click me</button>
+        </div>
+      );
     }
-  );
+  }
+);
 
   /*
     function Hello ({ foo, runMe }) {
