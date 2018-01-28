95d419eb27fe375100a5f86a3ac3088f045ec23f
Adam Schroder <aschroder@inlogik.com>
2018-01-28 21:55:09 +1100

    feat (proxy): found a way to infer type for react classes

--- packages/demos/todomvc-fluent/src/Hello.tsx
+++ packages/demos/todomvc-fluent/src/Hello.tsx
@@ -1,23 +1,22 @@
 import * as React from 'react';
 import { connect } from './app';
 
-var result = connect<{ baz: string }>()
+export default connect<{ baz: string }>()
   .with(({ state, signals, props }) => ({
       foo: state.foo,
       runMe: signals.test
-  }));
-
-export default result.to(
-  class Hello extends React.Component<typeof result.type> {
-    render () {
-      return (
-        <div>
-          <h1>Hello {this.props.foo}</h1>
-          <button onClick={e => this.props.runMe()}>Click me</button>
-        </div>
-      );
+  }))
+  .toClass((props) =>
+    class Hello extends React.Component<typeof props> {
+      render () {
+        return (
+          <div>
+            <h1>Hello {this.props.foo}</h1>
+            <button onClick={e => this.props.runMe()}>Click me</button>
+          </div>
+        );
+      }
     }
-  }
 );
 
   /*
