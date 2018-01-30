506d4fd22ead1a5e74611e55682579eac4d82d27
Christian Alfoni <christianalfoni@gmail.com>
2018-01-30 18:34:28 +0100

    feat(fluent): fixed state proxy handling and computed

---
+++ packages/demos/todomvc-fluent/src/module/computed.ts
@@ -0,0 +1,45 @@
+import { State } from './types';
+
+export function counts (state: State) {
+  return state.visibleTodosUids.get().reduce(
+    (counts, uid) => {
+      const todo = state.todos.get(uid);
+
+      if (todo && todo.completed) {
+        counts.completed++;
+      } else {
+        counts.remaining++;
+      }
+
+      counts.total++;
+
+      return counts;
+    },
+    {
+      completed: 0,
+      remaining: 0,
+      total: state.todos.values().length,
+      visible: state.visibleTodosUids.get().length,
+    }
+  );
+}
+export function  visibleTodosUids (state: State): string[] {
+  return state.todos.keys().filter(uid => {
+    const todo = state.todos.get(uid);
+
+    return (
+      state.filter === 'all' ||
+      (state.filter === 'completed' && todo && todo.completed) ||
+      (state.filter === 'active' && todo && !todo.completed)
+    );
+  });
+}
+export function  isAllChecked (state: State) {
+  return (
+    state.visibleTodosUids.get().filter(uid => {
+      const todo = state.todos.get(uid);
+
+      return todo && !todo.completed;
+    }).length === 0 && state.visibleTodosUids.get().length !== 0
+  );
+}
