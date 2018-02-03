e421ee5121a478d3a0e82724419c0124d434a2cf
Christian Alfoni <christianalfoni@gmail.com>
2018-02-03 20:17:48 +0100

    refactor(fluent): update names of exports and sequence handler

--- packages/demos/todomvc-fluent/src/ftree.ts
+++ packages/demos/todomvc-fluent/src/ftree.ts
@@ -1,38 +1,39 @@
 import { FunctionTree, Provider, } from 'function-tree';
-import { IContext, sequenceFactory } from 'function-tree/fluent'
-import { Devtools } from 'function-tree/devtools'
+import { IContext, sequenceFactory } from 'function-tree/fluent';
+import { Devtools } from 'function-tree/devtools';
 
 const devtools = new Devtools({
   host: ''
-})
+});
 
 type FooProvider = {
   get(test: string): string
-}
+};
 
 const provider: FooProvider = {
   get() {
-    return 'foo'
+    return 'foo';
   }
-}
+};
 
-const FooProvider = new Provider(provider)
+const FooProvider = new Provider(provider);
 
 const ft = new FunctionTree({
   foo: FooProvider
-})
+});
 
-devtools.watchExecution(ft)
+devtools.watchExecution(ft);
 
 interface AppContext<Props = {}> extends IContext<Props> {
-  foo: FooProvider
+  foo: FooProvider;
 }
 
 const sequence = sequenceFactory<AppContext, { foo: string}>(s => s
   .action(function ({ props, foo }) {
-    
+    return {};
   })
-)
+);
+
 ft.run(sequence, {
   bar: 'string'
-})
+});
