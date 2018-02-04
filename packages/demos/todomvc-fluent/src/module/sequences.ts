e0a8183ee79c05f2a92b951a3ab9ca993476529f
Adam Schroder <aschroder@inlogik.com>
2018-02-04 22:22:21 +1100

    refactor(fluent): make factories awesome

--- packages/demos/todomvc-fluent/src/module/sequences.ts
+++ packages/demos/todomvc-fluent/src/module/sequences.ts
@@ -1,35 +1,35 @@
-import { Sequence, SequenceWithProps } from '../globals';
+import { sequence, sequenceWithProps } from '../globals';
 import * as actions from './actions';
 
-export const redirectToAll = SequenceWithProps<{ filter: string }>(s => s
+export const redirectToAll = sequenceWithProps<{ filter: string }>(s => s
   .action(actions.redirectToAll)
 );
 
-export const changeNewTodoTitle = SequenceWithProps<{ title: string }>(s => s
+export const changeNewTodoTitle = sequenceWithProps<{ title: string }>(s => s
   .action(actions.changeNewTodoTitle)
 );
 
-export const removeTodo = SequenceWithProps<{ uid: string}>(s => s
+export const removeTodo = sequenceWithProps<{ uid: string}>(s => s
   .action(actions.removeTodo)
 );
 
-export const toggleAllChecked = Sequence(s => s
+export const toggleAllChecked = sequence(s => s
   .action(actions.toggleAllChecked)
 );
 
-export const toggleTodoCompleted = SequenceWithProps<{ uid: string}>(s => s
+export const toggleTodoCompleted = sequenceWithProps<{ uid: string}>(s => s
   .action(actions.toggleTodoCompleted)
 );
 
-export const clearCompletedTodos = Sequence(s => s
+export const clearCompletedTodos = sequence(s => s
   .action(actions.clearCompletedTodos)
 );
 
-export const changeFilter = SequenceWithProps<{ filter: string }>(s => s
+export const changeFilter = sequenceWithProps<{ filter: string }>(s => s
   .action(actions.changeFilter)
 );
 
-export const submitNewTodo = Sequence(s => s
+export const submitNewTodo = sequence(s => s
   .branch(actions.hasNewTodoTitle)
   .paths({ 
     true: s => s.action(actions.addTodo, actions.clearTodoTitle),
@@ -37,19 +37,19 @@ export const submitNewTodo = Sequence(s => s
   })
 );
 
-export const changeTodoTitle = SequenceWithProps<{ uid: string, title: string }>(s => s
+export const changeTodoTitle = sequenceWithProps<{ uid: string, title: string }>(s => s
   .action(actions.changeTodoTitle)
 );
 
-export const editTodo = SequenceWithProps<{ uid: string }>(s => s
+export const editTodo = sequenceWithProps<{ uid: string }>(s => s
   .action(actions.editTodo)
 );
 
-export const abortEdit = SequenceWithProps<{ uid: string}>(s => s
+export const abortEdit = sequenceWithProps<{ uid: string}>(s => s
   .action(actions.abortEditingTodo)
 );
 
-export const submitTodoTitle = SequenceWithProps<{ uid: string }>(s => s
+export const submitTodoTitle = sequenceWithProps<{ uid: string }>(s => s
   .branch(actions.whenEditedTitle)
   .paths({
     true: s => s.action(actions.updateTodoTitle),
