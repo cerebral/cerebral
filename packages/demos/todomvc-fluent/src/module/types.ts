8f6f58a78179f39c16ef1eb8dd565e97c588bb92
Adam Schroder <aschroder@inlogik.com>
2018-02-06 11:12:04 +1100

    fix(fluent): fix typings, format code and ensure routes are type safe

--- packages/demos/todomvc-fluent/src/module/types.ts
+++ packages/demos/todomvc-fluent/src/module/types.ts
@@ -2,26 +2,24 @@ import { Dictionary, ComputedValue } from '@cerebral/fluent';
 import * as signals from './sequences';
 
 export type Todo = {
-	title: string;
-	completed: boolean;
-	editedTitle: string;
+  title: string;
+  completed: boolean;
+  editedTitle: string;
 };
 
 export type State = {
-	newTodoTitle: string;
-	todos: Dictionary<Todo>;
-	filter: string;
-	editingUid: string | null;
-	counts: ComputedValue<
-		{
-			completed: number;
-			remaining: number;
-			total: number;
-			visible: number;
-		}
-	>;
-	visibleTodosUids: ComputedValue<string[]>;
-	isAllChecked: ComputedValue<boolean>;
+  newTodoTitle: string;
+  todos: Dictionary<Todo>;
+  filter: string;
+  editingUid: string | null;
+  counts: ComputedValue<{
+    completed: number;
+    remaining: number;
+    total: number;
+    visible: number;
+  }>;
+  visibleTodosUids: ComputedValue<string[]>;
+  isAllChecked: ComputedValue<boolean>;
 };
 
-export type Signals = { [key in keyof typeof signals]: typeof signals[key] };
+export type Signals = {[key in keyof typeof signals]: typeof signals[key]};
