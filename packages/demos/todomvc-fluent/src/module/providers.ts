ac7ac2b768530d17da858b51c144af8c8252bee1
Adam Schroder <aschroder@inlogik.com>
2018-02-12 17:12:39 +1100

    feat(http): add typings for http provider

--- packages/demos/todomvc-fluent/src/module/providers.ts
+++ packages/demos/todomvc-fluent/src/module/providers.ts
@@ -1,8 +1,11 @@
 import * as uuid from 'uuid';
 import { Provider } from '@cerebral/fluent';
+import HttpProvider from '@cerebral/http';
 
 export const id = Provider({
   create() {
     return uuid.v4();
   },
 });
+
+export const http = HttpProvider({});
