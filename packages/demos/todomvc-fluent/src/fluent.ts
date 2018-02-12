e708587fe3f1ba62e903ceb706d4f233b0447a8e
Adam Schroder <aschroder@inlogik.com>
2018-02-12 17:12:39 +1100

    feat(http): add typings for http provider

--- packages/demos/todomvc-fluent/src/fluent.ts
+++ packages/demos/todomvc-fluent/src/fluent.ts
@@ -1,12 +1,14 @@
 import { ConnectFactory, IContext, IBranchContext, SequenceFactory, SequenceWithPropsFactory } from '@cerebral/fluent';
 import { Provider as RouterProvider } from '@cerebral/router';
 import { State, Signals } from './module/types';
+import { HttpModule } from '@cerebral/http';
 
 interface Providers {
   id: {
     create(): string;
   };
   router: RouterProvider;
+  http: HttpModule;
 }
 
 export interface Context<Props = {}> extends IContext<Props>, Providers {
