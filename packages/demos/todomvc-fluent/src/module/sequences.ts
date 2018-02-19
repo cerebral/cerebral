cd4a2648a471fa595f30df81e1298b680804048f
Adam Schroder <aschroder@inlogik.com>
2018-02-19 13:01:15 +1100

    feat(fluent): add wait to builder and refactor branch api and docs

--- packages/demos/todomvc-fluent/src/module/sequences.ts
+++ packages/demos/todomvc-fluent/src/module/sequences.ts
@@ -9,7 +9,7 @@ export const changeNewTodoTitle = sequenceWithProps<{ title: string }>(s => s
   .action(actions.changeNewTodoTitle)
 );
 
-export const removeTodo = sequenceWithProps<{ uid: string}>(s => s
+export const removeTodo = sequenceWithProps<{ uid: string }>(s => s
   .action(actions.removeTodo)
 );
 
@@ -17,7 +17,7 @@ export const toggleAllChecked = sequence(s => s
   .action(actions.toggleAllChecked)
 );
 
-export const toggleTodoCompleted = sequenceWithProps<{ uid: string}>(s => s
+export const toggleTodoCompleted = sequenceWithProps<{ uid: string }>(s => s
   .action(actions.toggleTodoCompleted)
 );
 
@@ -31,7 +31,7 @@ export const changeFilter = sequenceWithProps<{ filter: string }>(s => s
 
 export const submitNewTodo = sequence(s => s
   .when(x => !!x.state.newTodoTitle)
-  .paths({ 
+  .paths({
     true: s => s.action(actions.addTodo, actions.clearTodoTitle),
     false: s => s
   })
@@ -45,12 +45,19 @@ export const editTodo = sequenceWithProps<{ uid: string }>(s => s
   .action(actions.editTodo)
 );
 
-export const abortEdit = sequenceWithProps<{ uid: string}>(s => s
+export const abortEdit = sequenceWithProps<{ uid: string }>(s => s
   .action(actions.abortEditingTodo)
 );
 
 export const submitTodoTitle = sequenceWithProps<{ uid: string }>(s => s
   .branch(actions.whenEditedTitle)
+  // .branch<{ true: {}, false: {} }>(x => {
+  //   const todo = x.state.todos.get(x.props.uid);
+  //   if (todo && todo.editedTitle) {
+  //     return x.path.true({});
+  //   }
+  //   return x.path.false({});
+  // })
   .paths({
     true: s => s.action(actions.updateTodoTitle),
     false: s => s
