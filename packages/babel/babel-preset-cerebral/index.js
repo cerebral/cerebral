ada3d58e573c7b35807e6d82b0995bfb1a023dca
Fabrice Weinberg <Fabrice@weinberg.me>
2018-04-05 17:36:48 +0200

    feat(proxy): transform dot syntax to tagged templates

--- packages/babel/babel-preset-cerebral/index.js
+++
@@ -1,9 +0,0 @@
-import proxyTags from 'babel-plugin-cerebral-proxy-tags'
-import optimizeTags from 'babel-plugin-cerebral-optimize-tags'
-
-export default function(context, opts = {}) {
-  const config = {
-    plugins: [opts.proxies && proxyTags, optimizeTags].filter(Boolean),
-  }
-  return config
-}
