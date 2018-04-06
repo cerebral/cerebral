70f44304715e42cc11a7801c65783997826d9f24
Christian Alfoni <christianalfoni@gmail.com>
2018-04-06 15:32:47 +0200

    refactor(todomvc): refactor to new babel plugin management

--- packages/demos/todomvc-ts/src/components/NewTodo/index.tsx
+++ packages/demos/todomvc-ts/src/components/NewTodo/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { connect } from '@cerebral/react'
-import { state, signal } from 'cerebral/proxy'
+import { state, signal } from 'fluent'
 
 export default connect({
   title: state.newTodoTitle,
