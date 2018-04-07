1c8256835685c6a9f54951b28efeb42a8ee33a65
Christian Alfoni <christianalfoni@gmail.com>
2018-04-07 17:00:35 +0200

    refactor(todomvc): use chaining API

--- packages/demos/todomvc-ts/src/app/sequences.ts
+++ packages/demos/todomvc-ts/src/app/sequences.ts
@@ -1,53 +1,86 @@
 import { redirect } from '@cerebral/router/operators'
-import { set, toggle, unset, when } from 'cerebral/operators'
-import { props, state } from 'cerebral/proxy'
+import { path, sequence, sequenceWithProps } from 'fluent'
 import * as actions from './actions'
 
-export const redirectToAll = redirect('/all')
+export const redirectToAll = sequence((s) => s.action(redirect('/all')))
 
-export const changeNewTodoTitle = set(state.newTodoTitle, props.title)
-
-export const removeTodo = unset(state.todos[props.uid])
-
-export const toggleAllChecked = actions.toggleAllChecked
-
-export const toggleTodoCompleted = toggle(state.todos[props.uid].completed)
-
-export const clearCompletedTodos = actions.clearCompletedTodos
-
-export const changeFilter = set(state.filter, props.filter)
-
-export const submitNewTodo = [
-  when(state.newTodoTitle),
-  {
-    true: [actions.addTodo, set(state.newTodoTitle, '')],
-    false: [],
-  },
-]
-
-export const changeTodoTitle = set(
-  state.todos[props.uid].editedTitle,
-  props.title
+export const changeNewTodoTitle = sequenceWithProps<{ title: string }>((s) =>
+  s.action(({ state, props }) => state.set(path.newTodoTitle, props.title))
 )
 
-export const editTodo = [
-  set(state.todos[props.uid].editedTitle, state.todos[props.uid].title),
-  set(state.editingUid, props.uid),
-]
+export const removeTodo = sequenceWithProps<{ uid: string }>((s) =>
+  s.action(({ state, props }) => state.unset(path.todos[props.uid]))
+)
 
-export const abortEdit = [
-  unset(state.todos[props.uid].editedTitle),
-  set(state.editingUid, null),
-]
+export const toggleAllChecked = sequence((s) =>
+  s.action(actions.toggleAllChecked)
+)
 
-export const submitTodoTitle = [
-  when(state.todos[props.uid].editedTitle),
-  {
-    true: [
-      set(state.todos[props.uid].title, state.todos[props.uid].editedTitle),
-      unset(state.todos[props.uid].editedTitle),
-      set(state.editingUid, null),
-    ],
-    false: [],
-  },
-]
+export const toggleTodoCompleted = sequenceWithProps<{ uid: string }>((s) =>
+  s.action(({ state, props }) => state.toggle(path.todos[props.uid].completed))
+)
+
+export const clearCompletedTodos = sequence((s) =>
+  s.action(actions.clearCompletedTodos)
+)
+
+export const changeFilter = sequenceWithProps<{ filter: string }>((s) =>
+  s.action(({ state, props }) => state.set(path.filter, props.filter))
+)
+
+export const submitNewTodo = sequence((s) =>
+  s.when(({ state }) => Boolean(state.get(path.newTodoTitle))).paths({
+    true: (s) =>
+      s
+        .action(actions.addTodo)
+        .action(({ state }) => state.set(path.newTodoTitle, '')),
+    false: (s) => s,
+  })
+)
+
+export const changeTodoTitle = sequenceWithProps<{
+  uid: string
+  title: string
+}>((s) =>
+  s.action(({ state, props }) =>
+    state.set(path.todos[props.uid].editedTitle, props.title)
+  )
+)
+
+export const editTodo = sequenceWithProps<{ uid: string }>((s) =>
+  s
+    .action(({ state, props }) =>
+      state.set(path.todos[props.uid].editedTitle, path.todos[props.uid].title)
+    )
+    .action(({ state, props }) => state.set(path.editingUid, props.uid))
+)
+
+export const abortEdit = sequenceWithProps<{ uid: string }>((s) =>
+  s
+    .action(({ state, props }) =>
+      state.unset(path.todos[props.uid].editedTitle)
+    )
+    .action(({ state }) => state.set(path.editingUid, null))
+)
+
+export const submitTodoTitle = sequenceWithProps<{ uid: string }>((s) =>
+  s
+    .when(({ state, props }) =>
+      Boolean(state.get(path.todos[props.uid].editedTitle))
+    )
+    .paths({
+      true: (s) =>
+        s
+          .action(({ state, props }) =>
+            state.set(
+              path.todos[props.uid].title,
+              path.todos[props.uid].editedTitle
+            )
+          )
+          .action(({ state, props }) =>
+            state.unset(path.todos[props.uid].editedTitle)
+          )
+          .action(({ state }) => state.set(path.editingUid, null)),
+      false: (s) => s,
+    })
+)
