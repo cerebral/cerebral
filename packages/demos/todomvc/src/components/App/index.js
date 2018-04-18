60b6f39fe120afa0a144ff35572397ab1ed4d095
Christian Alfoni <christianalfoni@gmail.com>
2018-04-18 21:41:59 +0200

    refactor(proxy): fix demo and react

--- packages/demos/todomvc/src/components/App/index.js
+++ packages/demos/todomvc/src/components/App/index.js
@@ -5,33 +5,30 @@ import TodosList from '../List'
 import TodosFooter from '../Footer'
 import computedCounts from '../../computed/counts'
 
-export default connect(
-  {
-    counts: computedCounts,
-  },
-  function App({ counts }) {
-    return (
-      <div id="todoapp-wrapper">
-        <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <NewTodoForm />
-          </header>
+export default connect({
+  counts: computedCounts,
+})(function App({ counts }) {
+  return (
+    <div id="todoapp-wrapper">
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTodoForm />
+        </header>
 
-          {!!counts.visible && <TodosList />}
-          {!!counts.total && <TodosFooter />}
-        </section>
-        <footer className="info">
-          <p>Double-click to edit a todo</p>
-          <p>
-            Credits:
-            <a href="http://christianalfoni.com">Christian Alfoni</a>,
-          </p>
-          <p>
-            Part of <a href="http://todomvc.com">TodoMVC</a>
-          </p>
-        </footer>
-      </div>
-    )
-  }
-)
+        {!!counts.visible && <TodosList />}
+        {!!counts.total && <TodosFooter />}
+      </section>
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>
+          Credits:
+          <a href="http://christianalfoni.com">Christian Alfoni</a>,
+        </p>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
+    </div>
+  )
+})
--- packages/demos/todomvc/src/components/Footer/index.js
+++ packages/demos/todomvc/src/components/Footer/index.js
@@ -6,46 +6,43 @@ import classnames from 'classnames'
 
 const filters = ['All', 'Active', 'Completed']
 
-export default connect(
-  {
-    filter: state.filter,
-    counts,
-    filterClicked: signal.filterClicked,
-    clearCompletedClicked: signal.clearCompletedClicked,
-  },
-  function Footer({ filter, counts, filterClicked, clearCompletedClicked }) {
-    return (
-      <footer className="footer">
-        <span className="todo-count">
-          <strong>
-            {counts.remaining} {counts.remaining === 1 ? 'item' : 'items'} left
-          </strong>
-        </span>
-        <ul className="filters">
-          {filters.map((filterName) => (
-            <li key={filterName}>
-              <a
-                onClick={() =>
-                  filterClicked({ filter: filterName.toLowerCase() })
-                }
-                className={classnames({
-                  selected: filter === filterName.toLowerCase(),
-                })}
-              >
-                {filterName}
-              </a>
-            </li>
-          ))}
-        </ul>
-        {!!counts.completed && (
-          <button
-            className="clear-completed"
-            onClick={() => clearCompletedClicked()}
-          >
-            Clear completed ({counts.completed})
-          </button>
-        )}
-      </footer>
-    )
-  }
-)
+export default connect({
+  filter: state.filter,
+  counts,
+  filterClicked: signal.filterClicked,
+  clearCompletedClicked: signal.clearCompletedClicked,
+})(function Footer({ filter, counts, filterClicked, clearCompletedClicked }) {
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>
+          {counts.remaining} {counts.remaining === 1 ? 'item' : 'items'} left
+        </strong>
+      </span>
+      <ul className="filters">
+        {filters.map((filterName) => (
+          <li key={filterName}>
+            <a
+              onClick={() =>
+                filterClicked({ filter: filterName.toLowerCase() })
+              }
+              className={classnames({
+                selected: filter === filterName.toLowerCase(),
+              })}
+            >
+              {filterName}
+            </a>
+          </li>
+        ))}
+      </ul>
+      {!!counts.completed && (
+        <button
+          className="clear-completed"
+          onClick={() => clearCompletedClicked()}
+        >
+          Clear completed ({counts.completed})
+        </button>
+      )}
+    </footer>
+  )
+})
