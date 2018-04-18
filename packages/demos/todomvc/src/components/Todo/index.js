a91eadc573ec0bd2b3b4d7de645a542916aee748
Christian Alfoni <christianalfoni@gmail.com>
2018-04-18 21:41:59 +0200

    refactor(proxy): fix demo and react

--- packages/demos/todomvc/src/components/Todo/index.js
+++ packages/demos/todomvc/src/components/Todo/index.js
@@ -3,66 +3,63 @@ import classnames from 'classnames'
 import { connect } from '@cerebral/react'
 import { state, signal, props } from 'cerebral/proxy'
 
-export default connect(
-  {
-    todo: state.todos[props.uid],
-    todoDoubleClicked: signal.todoDoubleClicked,
-    newTitleChanged: signal.todoNewTitleChanged,
-    newTitleSubmitted: signal.todoNewTitleSubmitted,
-    toggleCompletedChanged: signal.toggleTodoCompletedChanged,
-    removeTodoClicked: signal.removeTodoClicked,
-    newTitleAborted: signal.todoNewTitleAborted,
-  },
-  function Todo({
-    uid,
-    isEditing,
-    todo,
-    todoDoubleClicked,
-    newTitleChanged,
-    newTitleSubmitted,
-    toggleCompletedChanged,
-    removeTodoClicked,
-    newTitleAborted,
-  }) {
-    return (
-      <li
-        className={classnames({
-          completed: todo.completed,
-          editing: isEditing,
-        })}
-      >
-        <div className="view">
+export default connect({
+  todo: state.todos[props.uid],
+  todoDoubleClicked: signal.todoDoubleClicked,
+  newTitleChanged: signal.todoNewTitleChanged,
+  newTitleSubmitted: signal.todoNewTitleSubmitted,
+  toggleCompletedChanged: signal.toggleTodoCompletedChanged,
+  removeTodoClicked: signal.removeTodoClicked,
+  newTitleAborted: signal.todoNewTitleAborted,
+})(function Todo({
+  uid,
+  isEditing,
+  todo,
+  todoDoubleClicked,
+  newTitleChanged,
+  newTitleSubmitted,
+  toggleCompletedChanged,
+  removeTodoClicked,
+  newTitleAborted,
+}) {
+  return (
+    <li
+      className={classnames({
+        completed: todo.completed,
+        editing: isEditing,
+      })}
+    >
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          onChange={() => toggleCompletedChanged({ uid })}
+          checked={todo.completed}
+        />
+        <label onDoubleClick={() => todoDoubleClicked({ uid })}>
+          {todo.title}
+        </label>
+        <button
+          className="destroy"
+          onClick={() => removeTodoClicked({ uid })}
+        />
+      </div>
+      {isEditing && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            newTitleSubmitted({ uid })
+          }}
+        >
           <input
-            className="toggle"
-            type="checkbox"
-            onChange={() => toggleCompletedChanged({ uid })}
-            checked={todo.completed}
+            autoFocus
+            className="edit"
+            value={isEditing ? todo.editedTitle : todo.title}
+            onBlur={() => newTitleSubmitted({ uid })}
+            onChange={(e) => newTitleChanged({ uid, title: e.target.value })}
           />
-          <label onDoubleClick={() => todoDoubleClicked({ uid })}>
-            {todo.title}
-          </label>
-          <button
-            className="destroy"
-            onClick={() => removeTodoClicked({ uid })}
-          />
-        </div>
-        {isEditing && (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault()
-              newTitleSubmitted({ uid })
-            }}
-          >
-            <input
-              autoFocus
-              className="edit"
-              value={isEditing ? todo.editedTitle : todo.title}
-              onBlur={() => newTitleSubmitted({ uid })}
-              onChange={(e) => newTitleChanged({ uid, title: e.target.value })}
-            />
-          </form>
-        )}
-      </li>
-    )
-  }
-)
+        </form>
+      )}
+    </li>
+  )
+})
