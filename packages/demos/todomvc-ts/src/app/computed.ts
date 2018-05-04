e70f38f52ca551a0f0583aa6abe75f993504fbaa
Christian Alfoni <christianalfoni@Christians-MacBook-Pro.local>
2018-05-04 22:33:15 +0200

    test(Computed): finish tests and implementation

--- packages/demos/todomvc-ts/src/app/computed.ts
+++ packages/demos/todomvc-ts/src/app/computed.ts
@@ -1,52 +1,53 @@
 import { Compute } from 'cerebral'
 import { state, computed } from 'cerebral.proxy'
 
-export const visibleTodosUids = Compute(state.todos, state.filter)(
-  (todos, filter) => {
-    return Object.keys(todos).filter((uid) => {
-      return (
-        filter === 'all' ||
-        (filter === 'completed' && todos[uid].completed) ||
-        (filter === 'active' && !todos[uid].completed)
-      )
-    })
-  }
-)
-
-export const counts = Compute(state.todos, computed.visibleTodosUids)(
-  (todos, visibleTodosUids) => {
-    const todosUids = Object.keys(todos)
-
-    return todosUids.reduce(
-      (counts, uid) => {
-        if (todos[uid].completed) {
-          counts.completed++
-        } else {
-          counts.remaining++
-        }
-
-        counts.total++
-
-        return counts
-      },
-      {
-        completed: 0,
-        remaining: 0,
-        total: Object.keys(todos).length,
-        visible: todosUids.length,
-      }
-    )
-  }
-)
-
-export const isAllChecked = Compute(computed.visibleTodosUids)(
-  (visibleTodosUids, get) => {
+export const visibleTodosUids = Compute({
+  todos: state.todos,
+  filter: state.filter,
+})(({ todos, filter }) => {
+  return Object.keys(todos).filter((uid) => {
     return (
-      visibleTodosUids.filter((uid) => {
-        const todo = get(state.todos[uid])
-
-        return !todo.completed
-      }).length === 0 && visibleTodosUids.length !== 0
+      filter === 'all' ||
+      (filter === 'completed' && todos[uid].completed) ||
+      (filter === 'active' && !todos[uid].completed)
     )
-  }
-)
+  })
+})
+
+export const counts = Compute({
+  todos: state.todos,
+})(({ todos }) => {
+  const todosUids = Object.keys(todos)
+
+  return todosUids.reduce(
+    (counts, uid) => {
+      if (todos[uid].completed) {
+        counts.completed++
+      } else {
+        counts.remaining++
+      }
+
+      counts.total++
+
+      return counts
+    },
+    {
+      completed: 0,
+      remaining: 0,
+      total: Object.keys(todos).length,
+      visible: todosUids.length,
+    }
+  )
+})
+
+export const isAllChecked = Compute({
+  uids: computed.visibleTodosUids,
+})(({ uids }, get) => {
+  return (
+    uids.filter((uid) => {
+      const todo = get(state.todos[uid])
+
+      return !todo.completed
+    }).length === 0 && uids.length !== 0
+  )
+})
