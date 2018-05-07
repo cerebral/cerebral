02273caeb90e8fea4211e821c0396ede85bcf54c
Christian Alfoni <christianalfoni@gmail.com>
2018-05-07 11:28:45 +0200

    refactor(Controller): fix types and demo for operators

--- packages/demos/todomvc-ts/src/app/actions.ts
+++ packages/demos/todomvc-ts/src/app/actions.ts
@@ -1,27 +1,27 @@
 import { Context, state, computed } from 'cerebral.proxy'
 
-export function toggleAllChecked({ mutation, get }: Context) {
+export function toggleAllChecked({ operators, get }: Context) {
   const isCompleted = !get(computed.isAllChecked)
   const currentTodosUids = get(computed.visibleTodosUids)
 
   currentTodosUids.forEach((uid) => {
-    mutation.set(state.todos[uid].completed, isCompleted)
+    operators.set(state.todos[uid].completed, isCompleted)
   })
 }
 
-export function addTodo({ get, mutation, id }: Context) {
-  mutation.set(state.todos[id.create()], {
+export function addTodo({ get, operators, id }: Context) {
+  operators.set(state.todos[id.create()], {
     title: get(state.newTodoTitle),
     completed: false,
   })
 }
 
-export function clearCompletedTodos({ mutation, get }: Context) {
+export function clearCompletedTodos({ operators, get }: Context) {
   const todos = get(state.todos)
 
   Object.keys(todos).forEach((uid) => {
     if (todos[uid].completed) {
-      mutation.unset(state.todos[uid])
+      operators.unset(state.todos[uid])
     }
   })
 }
