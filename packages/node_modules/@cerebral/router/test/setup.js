e7e0c3ec2b8848be191e16af0d172ee86513f24f
christianalfoni <christianalfoni@gmail.com>
2018-05-29 20:19:29 +0200

    test(angular): remove angular test, no idea what is happening

---
+++ packages/node_modules/@cerebral/router/test/setup.js
@@ -0,0 +1,25 @@
+global.window = {
+  location: {
+    origin: 'http://localhost:3000',
+    href: 'http://localhost:3000/initial',
+  },
+  history: {},
+}
+global.history = {
+  pushState(_, __, value) {
+    window.location.href = window.location.origin + value
+    window.location.lastChangedWith = 'pushState'
+  },
+  replaceState(_, __, value) {
+    window.location.href = window.location.origin + value
+    window.location.lastChangedWith = 'replaceState'
+  },
+}
+global.addEventListener = global.window.addEventListener = () => {}
+global.window.CustomEvent = () => {}
+global.window.dispatchEvent = () => {}
+global.document = {}
+console.warn = function(message) {
+  console.warn.warnings.push(message)
+}
+console.warn.warnings = []
