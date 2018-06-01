90e421846932ab8bc8b53cc5e5178a2fbe23ebdc
christianalfoni <christianalfoni@gmail.com>
2018-06-01 21:52:25 +0200

    refactor(monorepo): fix linting

--- packages/demos/todomvc-ts/src/components/Todo/index.tsx
+++ packages/demos/todomvc-ts/src/components/Todo/index.tsx
@@ -18,52 +18,55 @@ const deps = {
   abortEdit: sequences.abortEdit,
 }
 
-export default connect<ExternalProps, typeof deps>(deps, function Todo({
-  uid,
-  isEditing,
-  todo,
-  todoDoubleClicked,
-  changeTodoTitle,
-  submitTodoTitle,
-  toggleTodoCompleted,
-  removeTodo,
-  abortEdit,
-}) {
-  return (
-    <li
-      className={classnames({
-        completed: todo.completed,
-        editing: isEditing,
-      })}
-    >
-      <div className="view">
-        <input
-          className="toggle"
-          type="checkbox"
-          onChange={() => toggleTodoCompleted({ uid })}
-          checked={todo.completed}
-        />
-        <label onDoubleClick={() => todoDoubleClicked({ uid })}>
-          {todo.title}
-        </label>
-        <button className="destroy" onClick={() => removeTodo({ uid })} />
-      </div>
-      {isEditing && (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            submitTodoTitle({ uid })
-          }}
-        >
+export default connect<ExternalProps, typeof deps>(
+  deps,
+  function Todo({
+    uid,
+    isEditing,
+    todo,
+    todoDoubleClicked,
+    changeTodoTitle,
+    submitTodoTitle,
+    toggleTodoCompleted,
+    removeTodo,
+    abortEdit,
+  }) {
+    return (
+      <li
+        className={classnames({
+          completed: todo.completed,
+          editing: isEditing,
+        })}
+      >
+        <div className="view">
           <input
-            autoFocus
-            className="edit"
-            value={isEditing ? todo.editedTitle : todo.title}
-            onBlur={() => submitTodoTitle({ uid })}
-            onChange={(e) => changeTodoTitle({ uid, title: e.target.value })}
+            className="toggle"
+            type="checkbox"
+            onChange={() => toggleTodoCompleted({ uid })}
+            checked={todo.completed}
           />
-        </form>
-      )}
-    </li>
-  )
-})
+          <label onDoubleClick={() => todoDoubleClicked({ uid })}>
+            {todo.title}
+          </label>
+          <button className="destroy" onClick={() => removeTodo({ uid })} />
+        </div>
+        {isEditing && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              submitTodoTitle({ uid })
+            }}
+          >
+            <input
+              autoFocus
+              className="edit"
+              value={isEditing ? todo.editedTitle : todo.title}
+              onBlur={() => submitTodoTitle({ uid })}
+              onChange={(e) => changeTodoTitle({ uid, title: e.target.value })}
+            />
+          </form>
+        )}
+      </li>
+    )
+  }
+)
